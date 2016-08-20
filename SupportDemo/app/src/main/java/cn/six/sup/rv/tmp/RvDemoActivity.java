73b399d8e0c27df8bfeff2c6ee46bda2abfe00ce
songzhw <songzhw2012@gmail.com>
2016-08-20 00:20:39 -0400

    build my own RecyclerViewActivity template for the Android Studio

---
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/tmp/RvDemoActivity.java
@@ -0,0 +1,55 @@
+package cn.six.sup.rv.tmp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.six.sup.rv.RvViewHolder;
+import cn.six.sup.rv.OnRvItemClickListener;
+import cn.six.sup.rv.one_adapter.OneAdapter;
+
+import cn.six.sup.R;
+import cn.six.sup.rv.tmp.data.TempData;
+
+public class RvDemoActivity extends AppCompatActivity {
+    private RecyclerView rv;
+    private OneAdapter<TempData> adapter;
+    private List<TempData> data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rv_demo);
+
+        rv = (RecyclerView) findViewById(R.id.rv_data);
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new OneAdapter<TempData>(R.layout.item_rv_demo) {
+            @Override
+            protected void apply(RvViewHolder vh, TempData value, int position) {
+
+            }
+        };
+        adapter.data = data;
+        rv.setAdapter(adapter);
+
+        rv.addOnItemTouchListener(new OnRvItemClickListener(rv) {
+            @Override
+            public void onLongClick(RecyclerView.ViewHolder vh) {
+            }
+
+            @Override
+            public void onItemClick(RecyclerView.ViewHolder vh) {
+
+            }
+        });
+
+        // ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new RvItemTouchHelperCallback(this));
+        // itemTouchHelper.attachToRecyclerView(rv);        
+    }
+}
---
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/tmp/data/TempData.java
@@ -0,0 +1,7 @@
+package cn.six.sup.rv.tmp.data;
+
+/**
+ * Created by songzhw on 2016-08-20
+ */
+public class TempData {
+}
