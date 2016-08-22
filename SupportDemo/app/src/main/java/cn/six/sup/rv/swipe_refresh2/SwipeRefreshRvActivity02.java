bfd45ecc18b684bb8e2318dac65d66870eb966a1
songzhw <songzhw2012@gmail.com>
2016-08-22 13:32:12 -0400

    1. add LoadMore 2. fix bug : the adapter does not refresh after the SwipeToRefresh

--- SupportDemo/app/src/main/java/cn/six/sup/rv/swipe_refresh2/SwipeRefreshRvActivity02.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/swipe_refresh2/SwipeRefreshRvActivity02.java
@@ -7,21 +7,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import cn.six.sup.rv.RvViewHolder;
 import cn.six.sup.rv.OnRvItemClickListener;
+import cn.six.sup.rv.load_more.ILoadMoreListener;
+import cn.six.sup.rv.load_more.LoadMoreWrapper;
 import cn.six.sup.rv.one_adapter.OneAdapter;
 
 import cn.six.sup.R;
 
 // only "swipe to refresh"
-public class SwipeRefreshRvActivity02 extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+public class SwipeRefreshRvActivity02 extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener, ILoadMoreListener {
     private SwipeRefreshLayout slay;
     private RecyclerView rv;
     private OneAdapter<String> adapter;
+    private LoadMoreWrapper wrapper;
     private List<String> data;
 
     @Override
@@ -47,13 +54,20 @@ public class SwipeRefreshRvActivity02 extends AppCompatActivity implements Swipe
             data.add("Item : "+i);
         }
         adapter.data = data;
-        rv.setAdapter(adapter);
+//        rv.setAdapter(adapter);
+
+        TextView tv = new TextView(this);
+        tv.setText("Loading ...");
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 22);
+        wrapper = new LoadMoreWrapper(adapter, this);
+        wrapper.loadMoreView = tv;
+        rv.setAdapter(wrapper);
     }
 
     @Override
     public void onRefresh() {
         // Mock the http
-        handler.sendEmptyMessageDelayed(11, 4000);
+        handler.sendEmptyMessageDelayed(11, 1000);
     }
 
     private Handler handler = new Handler(){
@@ -61,10 +75,21 @@ public class SwipeRefreshRvActivity02 extends AppCompatActivity implements Swipe
         public void handleMessage(Message msg) {
             if(msg.what == 11){
                 data.add(0, "Item : "+ System.currentTimeMillis());
-                adapter.notifyItemInserted(0);
+                adapter.data = data;
+                wrapper.notifyDataSetChanged(); // important!
                 rv.smoothScrollToPosition(0); // move to item 0 for better UX
                 slay.setRefreshing(false);
             }
         }
     };
+
+    @Override
+    public boolean hasMore() {
+        return false;
+    }
+
+    @Override
+    public void onLoadMore() {
+
+    }
 }
