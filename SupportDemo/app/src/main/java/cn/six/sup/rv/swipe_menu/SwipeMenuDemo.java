3ff78763257f078badc81373042c9393c1189b1a
songzhw <songzhw2012@gmail.com>
2016-09-08 16:04:11 -0400

    Rv with SwipeItemMenu 4: add Rv

--- SupportDemo/app/src/main/java/cn/six/sup/rv/swipe_menu/SwipeMenuDemo.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/swipe_menu/SwipeMenuDemo.java
@@ -19,43 +19,9 @@ import cn.six.sup.rv.header.HeaderWrapper;
 import cn.six.sup.R;
 
 public class SwipeMenuDemo extends AppCompatActivity {
-    private RecyclerView rv;
-    private OneAdapter<String> adapter;
-    private List<String> data;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_swipe_menu_demo);
-
-
-//
-//        setContentView(R.layout.activity_swipe_menu_demo);
-//
-//        rv = (RecyclerView) findViewById(R.id.rv_data);
-//        rv.setHasFixedSize(true);
-//        rv.setLayoutManager(new LinearLayoutManager(this));
-//
-//        adapter = new OneAdapter<String>(R.layout.item_swipe_menu_demo) {
-//            @Override
-//            protected void apply(RvViewHolder vh, String value, int position) {
-//
-//            }
-//        };
-//        adapter.data = data;
-//        rv.setAdapter(adapter);
-//
-//
-//        rv.addOnItemTouchListener(new OnRvItemClickListener(rv) {
-//            @Override
-//            public void onLongClick(RecyclerView.ViewHolder vh) {
-//            }
-//
-//            @Override
-//            public void onItemClick(RecyclerView.ViewHolder vh) {
-//
-//                adapter.notifyDataSetChanged();
-//            }
-//        });
     }
 }
---
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/swipe_menu/rv/SwipeMenuRvDemo.java
@@ -0,0 +1,62 @@
+package cn.six.sup.rv.swipe_menu.rv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+import cn.six.sup.rv.RvViewHolder;
+import cn.six.sup.rv.OnRvItemClickListener;
+import cn.six.sup.rv.one_adapter.OneAdapter;
+import cn.six.sup.rv.header.HeaderWrapper;
+
+import cn.six.sup.R;
+
+public class SwipeMenuRvDemo extends AppCompatActivity {
+    private RecyclerView rv;
+    private OneAdapter<String> adapter;
+    private List<String> data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_menu_demo);
+
+        rv = (RecyclerView) findViewById(R.id.rv_data);
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new OneAdapter<String>(R.layout.item_swipe_menu_demo) {
+            @Override
+            protected void apply(RvViewHolder vh, String value, int position) {
+                vh.setText(R.id.tvRvItemSwipe, value);
+            }
+        };
+        data = new ArrayList<>();
+        for(int i = 0 ; i < 20; i++) {
+            data.add("Item "+i);
+        }
+        adapter.data = data;
+        rv.setAdapter(adapter);
+        System.out.println("szw adapter.count = "+adapter.getItemCount());
+
+
+        rv.addOnItemTouchListener(new OnRvItemClickListener(rv) {
+            @Override
+            public void onLongClick(RecyclerView.ViewHolder vh) {
+            }
+
+            @Override
+            public void onItemClick(RecyclerView.ViewHolder vh) {
+
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
