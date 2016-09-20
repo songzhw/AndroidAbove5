37a0d0655b3fe6ef15a0048e6a40463b8254b0e0
songzhw <songzhw2012@gmail.com>
2016-09-20 19:42:24 -0400

    Customize Quick Settings Tile in Android 7 : update the icon and lable when you click it

--- Android7x/app/src/main/java/ca/six/and7/tile/HelloTile.java
+++ Android7x/app/src/main/java/ca/six/and7/tile/HelloTile.java
@@ -1,13 +1,21 @@
 package ca.six.and7.tile;
 
+import android.graphics.drawable.Icon;
+import android.net.NetworkInfo;
+import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import android.widget.Toast;
+
+import ca.six.and7.R;
 
 /**
  * Created by songzhw on 2016-09-20
  */
 
 public class HelloTile extends TileService {
+    private final int STATE_OFF = 0;
+    private final int STATE_ON = 1;
+    private int toggleState = STATE_ON;
 
     @Override
     public void onTileAdded() {
@@ -36,7 +44,24 @@ public class HelloTile extends TileService {
     @Override
     public void onClick() {
         super.onClick();
-        System.out.println("szw onClick()");
-        Toast.makeText(this, "Clicked Me!", Toast.LENGTH_SHORT).show();
+        Tile tile = getQsTile();
+        Icon icon;
+        if (toggleState == STATE_ON) {
+            toggleState = STATE_OFF;
+            icon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_lock_lock);
+            tile.setState(Tile.STATE_INACTIVE);
+            tile.setLabel("Hmmm");
+        } else {
+            toggleState = STATE_ON;
+            icon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_lock_open);
+            tile.setState(Tile.STATE_ACTIVE);
+            tile.setLabel("Hello");
+        }
+
+        tile.setIcon(icon);
+        tile.updateTile();
+        // 无论你传的什么颜色的Icon,STATE_ACTIVE都会把Icon渲染成白色, STATE_INACTIVE都会把Icon渲染成灰色。
+        // but you can still click the tile whose state is STATE_INACTIVE
+        // (It's like wifi and close wifi, but you can still click the tile)
     }
 }
