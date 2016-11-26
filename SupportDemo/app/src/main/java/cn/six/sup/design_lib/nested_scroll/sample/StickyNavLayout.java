93a8cc2280e701f7f7a18ec00f72347bc0881578
songzhw <songzhw2012@gmail.com>
2016-11-26 13:22:04 -0500

    Nested Scrolling

--- SupportDemo/app/src/main/java/cn/six/sup/design_lib/nested_scroll/sample/StickyNavLayout.java
+++ SupportDemo/app/src/main/java/cn/six/sup/design_lib/nested_scroll/sample/StickyNavLayout.java
@@ -8,7 +8,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.VelocityTracker;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.OverScroller;
@@ -24,98 +23,13 @@ public class StickyNavLayout extends LinearLayout implements NestedScrollingPare
     private ViewPager viewPager;
 
     private int topViewHeight;
-
     private OverScroller overScroller;
-    private VelocityTracker velocityTracker;
-    private int touchSlop;
-    private int maximumVelocity, minimumVelocity;
-
-    private float mLastY;
-    private boolean mDragging;
-
-
-    @Override
-    public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
-        Log.e(TAG, "onStartNestedScroll");
-        return true;
-    }
-
-    @Override
-    public void onNestedScrollAccepted(View child, View target, int nestedScrollAxes) {
-        Log.e(TAG, "onNestedScrollAccepted");
-    }
-
-    @Override
-    public void onStopNestedScroll(View target) {
-        Log.e(TAG, "onStopNestedScroll");
-    }
-
-    @Override
-    public void onNestedScroll(View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        Log.e(TAG, "onNestedScroll");
-    }
-
-    @Override
-    public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
-        Log.e(TAG, "onNestedPreScroll");
-        boolean hiddenTop = dy > 0 && getScrollY() < topViewHeight;
-        boolean showTop = dy < 0 && getScrollY() >= 0 && !ViewCompat.canScrollVertically(target, -1);
-
-        if (hiddenTop || showTop) {
-            scrollBy(0, dy);
-            consumed[1] = dy;
-        }
-    }
-
-    @Override
-    public boolean onNestedFling(View target, float velocityX, float velocityY, boolean consumed) {
-        Log.e(TAG, "onNestedFling");
-        return false;
-    }
-
-    @Override
-    public boolean onNestedPreFling(View target, float velocityX, float velocityY) {
-        Log.e(TAG, "onNestedPreFling");
-        //down - //up+
-        if (getScrollY() >= topViewHeight) return false;
-        fling((int) velocityY);
-        return true;
-    }
-
-    @Override
-    public int getNestedScrollAxes() {
-        Log.e(TAG, "getNestedScrollAxes");
-        return 0;
-    }
-
-
 
     public StickyNavLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         setOrientation(LinearLayout.VERTICAL);
-
         overScroller = new OverScroller(context);
-        touchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
-        maximumVelocity = ViewConfiguration.get(context)
-                .getScaledMaximumFlingVelocity();
-        minimumVelocity = ViewConfiguration.get(context)
-                .getScaledMinimumFlingVelocity();
-
     }
-
-    private void initVelocityTrackerIfNotExists() {
-        if (velocityTracker == null) {
-            velocityTracker = VelocityTracker.obtain();
-        }
-    }
-
-    private void recycleVelocityTracker() {
-        if (velocityTracker != null) {
-            velocityTracker.recycle();
-            velocityTracker = null;
-        }
-    }
-
 
     @Override
     protected void onFinishInflate() {
@@ -124,8 +38,7 @@ public class StickyNavLayout extends LinearLayout implements NestedScrollingPare
         navView = findViewById(R.id.id_stickynavlayout_indicator);
         View view = findViewById(R.id.id_stickynavlayout_viewpager);
         if (!(view instanceof ViewPager)) {
-            throw new RuntimeException(
-                    "id_stickynavlayout_viewpager show used by ViewPager !");
+            throw new RuntimeException("id_stickynavlayout_viewpager show used by ViewPager !");
         }
         viewPager = (ViewPager) view;
     }
@@ -135,8 +48,10 @@ public class StickyNavLayout extends LinearLayout implements NestedScrollingPare
         //不限制顶部的高度
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         getChildAt(0).measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+
         ViewGroup.LayoutParams params = viewPager.getLayoutParams();
         params.height = getMeasuredHeight() - navView.getMeasuredHeight();
+
         setMeasuredDimension(getMeasuredWidth(), topView.getMeasuredHeight() + navView.getMeasuredHeight() + viewPager.getMeasuredHeight());
 
     }
@@ -174,5 +89,59 @@ public class StickyNavLayout extends LinearLayout implements NestedScrollingPare
         }
     }
 
+
+    @Override
+    public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
+        Log.e(TAG, "onNestedPreScroll");
+        boolean hiddenTop = dy > 0 && getScrollY() < topViewHeight;
+        boolean showTop = dy < 0 && getScrollY() >= 0 && !ViewCompat.canScrollVertically(target, -1);
+
+        if (hiddenTop || showTop) {
+            scrollBy(0, dy);
+            consumed[1] = dy;
+        }
+    }
+
+    @Override
+    public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
+        Log.e(TAG, "onStartNestedScroll");
+        return true;
+    }
+
+    @Override
+    public boolean onNestedFling(View target, float velocityX, float velocityY, boolean consumed) {
+        Log.e(TAG, "onNestedFling");
+        return false;
+    }
+
+    @Override
+    public boolean onNestedPreFling(View target, float velocityX, float velocityY) {
+        Log.e(TAG, "onNestedPreFling");
+        //down - //up+
+        if (getScrollY() >= topViewHeight) return false;
+        fling((int) velocityY);
+        return true;
+    }
+
+    @Override
+    public int getNestedScrollAxes() {
+        Log.e(TAG, "getNestedScrollAxes");
+        return 0;
+    }
+
+    @Override
+    public void onNestedScrollAccepted(View child, View target, int nestedScrollAxes) {
+        Log.e(TAG, "onNestedScrollAccepted");
+    }
+
+    @Override
+    public void onStopNestedScroll(View target) {
+        Log.e(TAG, "onStopNestedScroll");
+    }
+
+    @Override
+    public void onNestedScroll(View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        Log.e(TAG, "onNestedScroll");
+    }
 
 }
