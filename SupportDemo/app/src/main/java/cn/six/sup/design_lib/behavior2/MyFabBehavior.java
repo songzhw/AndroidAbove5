55292879330c5b92b8fbb55521505d238cef3bc6
songzhw <songzhw2012@gmail.com>
2016-12-12 23:15:13 -0500

    clay + Behavior

--- SupportDemo/app/src/main/java/cn/six/sup/design_lib/behavior2/MyFabBehavior.java
+++ SupportDemo/app/src/main/java/cn/six/sup/design_lib/behavior2/MyFabBehavior.java
@@ -25,14 +25,22 @@ public class MyFabBehavior extends FloatingActionButton.Behavior {
     }
 
     // 检查Y轴的距离，决定是显示还是隐藏FAB
+
     @Override
-    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton childFab,
                                View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
-        if(dyConsumed > 0 && child.getVisibility() == View.VISIBLE){
-            child.hide();
-        } else if(dyConsumed < 0 && child.getVisibility() != View.VISIBLE){
-            child.show();
+        super.onNestedScroll(coordinatorLayout, childFab, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+
+        // target : (即dependency)  NestedScrollView
+        // 因为此例中， rv这个child的滑动， 并不要求parent也滑动。 所以d*Unconsumer全是0,
+        // 即child view全滑动了，parent不要求滑动
+        System.out.println("szw consumed : x = "+dxConsumed + " ; y = "+dyConsumed);
+        System.out.println("szw unconsumed : x = "+dxUnconsumed + " ; y = "+dyUnconsumed);
+
+        if(dyConsumed > 0 && childFab.getVisibility() == View.VISIBLE){
+            childFab.hide();
+        } else if(dyConsumed < 0 && childFab.getVisibility() != View.VISIBLE){
+            childFab.show();
         }
     }
 
