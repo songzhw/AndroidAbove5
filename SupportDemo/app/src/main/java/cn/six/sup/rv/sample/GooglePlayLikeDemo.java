419abc1921e79af8056fb9dfde63e47a60913333
songzhw <songzhw2012@gmail.com>
2016-12-14 20:55:52 -0500

    Google_Play-like demo : CoordinatorLayout and AppBarLayout, seems have problems here. Remove them

--- SupportDemo/app/src/main/java/cn/six/sup/rv/sample/GooglePlayLikeDemo.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/sample/GooglePlayLikeDemo.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.List;
