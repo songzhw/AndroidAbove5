4efaa12e83e69ffe88f083a56a7fe4261ca7e2e0
songzhw <songzhw2012@gmail.com>
2016-12-14 20:42:30 -0500

    Google_Play-like demo

--- SupportDemo/app/src/main/java/cn/six/sup/rv/sample/GooglePlayLikeDemo.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/sample/GooglePlayLikeDemo.java
@@ -2,6 +2,7 @@ package cn.six.sup.rv.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -10,6 +11,8 @@ import java.util.List;
 import cn.six.sup.R;
 import cn.six.sup.rv.RvViewHolder;
 import cn.six.sup.rv.one_adapter.OneAdapter;
+
+import static android.support.v7.widget.LinearLayoutManager.HORIZONTAL;
 
 /**
  * Created by songzhw on 2016-12-14
@@ -20,9 +23,10 @@ public class GooglePlayLikeDemo extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recycler);
+        setContentView(R.layout.activity_play_like);
 
         RecyclerView rv = (RecyclerView) findViewById(R.id.rvRefresh);
+        rv.setLayoutManager(new LinearLayoutManager(this));
         OneAdapter<List<Integer>> adapter = new OneAdapter<List<Integer>>(R.layout.item_play_like_middle) {
             @Override
             protected void apply(RvViewHolder vh, List<Integer> integers, int position) {
@@ -31,7 +35,7 @@ public class GooglePlayLikeDemo extends Activity {
         };
 
         List<List<Integer>> data = new ArrayList<>();
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 30; i++){
             List<Integer> innerData = new ArrayList<>();
             for(int j = 0; j < 10; j++){
                 if(j % 2 == 0) {
@@ -40,6 +44,7 @@ public class GooglePlayLikeDemo extends Activity {
                     innerData.add(R.drawable.in_icon);
                 }
             }
+            data.add(innerData);
         }
         adapter.data = data;
         rv.setAdapter(adapter);
@@ -47,6 +52,7 @@ public class GooglePlayLikeDemo extends Activity {
 
     private void applyInnerRv(RvViewHolder vh, List<Integer> integers, int position) {
         RecyclerView rvInner = vh.getView(R.id.rvMiddle);
+        rvInner.setLayoutManager(new LinearLayoutManager(this, HORIZONTAL, false));
         OneAdapter<Integer> adapterInner = new OneAdapter<Integer>(R.layout.item_play_like) {
             @Override
             protected void apply(RvViewHolder vh, Integer integer, int position) {
