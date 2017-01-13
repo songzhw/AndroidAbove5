8171485cd1520914ad7d6d14ab3e51850d3c18f4
songzhw <songzhw2012@gmail.com>
2017-01-13 16:06:14 -0500

    Chrome Custom Tab : action button, menu item, enter/exit anim

--- SupportDemo/app/src/main/java/cn/six/sup/other/chrome/ChromeJumperDemo.java
+++ SupportDemo/app/src/main/java/cn/six/sup/other/chrome/ChromeJumperDemo.java
@@ -1,11 +1,17 @@
 package cn.six.sup.other.chrome;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.customtabs.CustomTabsIntent;
 import android.view.View;
 import android.widget.Button;
+
+import cn.six.sup.R;
 
 /**
  * Created by songzhw on 2017-01-13
@@ -26,7 +32,28 @@ public class ChromeJumperDemo extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         String url = "https://github.com/songzhw";
         CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+
+        builder.setToolbarColor(getResources().getColor(R.color.colorPrimary));
+
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_pets); // 图标的高度一般为24dp，宽度一般为24-48dp。
+        PendingIntent pendingIntent = createPendingIntent(ActionBroadcastReceiver.ACTION_ACTION_BUTTON);
+        builder.setActionButton(icon, "Pets", pendingIntent);
+
+        builder.addMenuItem("toast 01", pendingIntent);
+        builder.addMenuItem("toast 02", pendingIntent);
+
+//        builder.setStartAnimations(this, R.anim.slide_in_right, R.anim.slide_out_left);
+//        builder.setExitAnimations(this, R.anim.slide_in_left, R.anim.slide_out_right);
+
         CustomTabsIntent customTabsIntent = builder.build();
         customTabsIntent.launchUrl(this, Uri.parse(url));  //加载url
+    }
+
+    private PendingIntent createPendingIntent(int actionSourceId) {
+        Intent actionIntent = new Intent(
+                this.getApplicationContext(), ActionBroadcastReceiver.class);
+        actionIntent.putExtra(ActionBroadcastReceiver.KEY_ACTION_SOURCE, actionSourceId);
+        return PendingIntent.getBroadcast(
+                getApplicationContext(), actionSourceId, actionIntent, 0);
     }
 }
