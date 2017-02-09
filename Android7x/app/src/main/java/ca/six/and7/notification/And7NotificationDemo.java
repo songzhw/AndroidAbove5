f5451e45de24edc67858dab0671b13f625b17e5f
songzhw <songzhw2012@gmail.com>
2017-02-08 19:21:24 -0500

    And7 : Notification

--- Android7x/app/src/main/java/ca/six/and7/notification/And7NotificationDemo.java
+++ Android7x/app/src/main/java/ca/six/and7/notification/And7NotificationDemo.java
@@ -2,9 +2,14 @@ package ca.six.and7.notification;
 
 import android.app.Activity;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.RemoteInput;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
 
@@ -36,19 +41,43 @@ public class And7NotificationDemo extends Activity {
         mgr.notify(id, builder.build());
     }
 
-    public void click02(View v){
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-            .setSmallIcon(R.drawable.ic_pets)
-            .setContentTitle("Notification here")
-            .setContentText("Second news. Hello World")
-            .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher))
-            .setColor(ContextCompat.getColor(this, R.color.colorAccent))
-            .setGroupSummary(true)
-            .setGroup(notificationGroupID)
-            .setAutoCancel(true);
 
-        int id = 2;
-        NotificationManager mgr = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+
+
+    public static final String KEY_REPLY = "KEY_REPLY";
+    public static final String ACTION_REPLY = "ACTION_REPLY";
+
+    public void click02(View v){
+        int id = 222;
+
+        RemoteInput replyRemoteInput = new RemoteInput.Builder(KEY_REPLY)
+                .setLabel("input here to reply") // hint
+                .build();
+        NotificationCompat.Action replyAction = new NotificationCompat.Action.Builder(
+                R.drawable.ic_reply, "reply", getReplyPendingIntent(this, id))
+                .addRemoteInput(replyRemoteInput)
+                .setAllowGeneratedReplies(true)
+                . build();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_pets)
+                .setContentTitle("Notification here")
+                .setContentText("Second news. Hello World")
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher))
+                .setColor(ContextCompat.getColor(this, R.color.colorAccent))
+                .setGroup(notificationGroupID)
+                .setGroupSummary(true)
+                .setContentIntent(PendingIntent.getActivity(this, 0, new Intent(), 0))
+                .addAction(replyAction)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat mgr = NotificationManagerCompat.from(this);
         mgr.notify(id, builder.build());
+    }
+
+    private PendingIntent getReplyPendingIntent(Context ctx, int notificationId ){
+        Intent it = ReplyBroadcastReceiver.getReplyIntent( notificationId);
+        return PendingIntent.getBroadcast(ctx, 100, it, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
---
+++ Android7x/app/src/main/java/ca/six/and7/notification/ReplyBroadcastReceiver.java
@@ -0,0 +1,51 @@
+package ca.six.and7.notification;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.RemoteInput;
+import android.widget.Toast;
+
+import ca.six.and7.R;
+
+public class ReplyBroadcastReceiver extends BroadcastReceiver {
+    private static final String KEY_NOTIFICATION_ID = "key_notify_id";
+
+    @Override
+    public void onReceive(Context context, Intent it) {
+        if (And7NotificationDemo.ACTION_REPLY.equalsIgnoreCase(it.getAction())){
+            CharSequence cs = getReplyMessage(it);
+            Toast.makeText(context, "Message = "+cs, Toast.LENGTH_LONG).show();
+
+            int notificationId = it.getIntExtra(KEY_NOTIFICATION_ID, 0);
+            updateNotification(context, notificationId);
+        }
+    }
+
+    private CharSequence getReplyMessage(Intent it){
+        Bundle remoteInputBundle = RemoteInput.getResultsFromIntent(it);
+        if(remoteInputBundle != null){
+            return remoteInputBundle.getCharSequence(And7NotificationDemo.KEY_REPLY);
+        }
+        return null;
+    }
+
+    public static Intent getReplyIntent(int notificationId) {
+        Intent it = new Intent(And7NotificationDemo.ACTION_REPLY);
+        it.putExtra(KEY_NOTIFICATION_ID, notificationId); // 后面更新要用
+        return it;
+    }
+
+
+    private void updateNotification(Context context, int notificationId) {
+        NotificationManagerCompat mgr = NotificationManagerCompat.from(context);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+            .setSmallIcon(R.drawable.ic_done)
+            .setContentText("Done");
+        mgr.notify(notificationId, builder.build());
+
+    }
+}
