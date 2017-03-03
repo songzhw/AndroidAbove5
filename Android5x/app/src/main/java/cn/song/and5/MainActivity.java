3bcaaa90af2e2d0de58b49c5ca62372335e3b1f0
songzhw <songzhw2012@gmail.com>
2017-03-02 21:36:20 -0500

    fix bug

--- Android5x/app/src/main/java/cn/song/and5/MainActivity.java
+++ Android5x/app/src/main/java/cn/song/and5/MainActivity.java
@@ -1,17 +1,18 @@
 package cn.song.and5;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.StateListAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import cn.song.and5.anim.actv.ExplodeActivit01y;
 import cn.song.and5.anim.musicplayer.ListPage;
 import cn.song.and5.anim.transition.AnimFirstActivity;
 import cn.song.and5.anim.ViewCircularRevealActivity;
-import cn.song.and5.rv.cards.RvCardsActivity;
-import cn.song.and5.rv.simple.RvSimpleTestActivity;
-import cn.song.and5.rv.waterfall.RvWaterFallActivity;
 import cn.song.and5.snackbar.SnackBarTestActivity;
 
 public class MainActivity extends Activity {
@@ -21,11 +22,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void clickWaterfall(View v){
-        Intent it = new Intent(this, RvWaterFallActivity.class);
-        startActivity(it);
+//        Intent it = new Intent(this, RvWaterFallActivity.class);
+//        startActivity(it);
     }
 
     public void clickAnimTest(View v){
@@ -44,13 +46,13 @@ public class MainActivity extends Activity {
     }
 
     public void clickSimpleRv(View v){
-        Intent it = new Intent(this, RvSimpleTestActivity.class);
-        startActivity(it);
+//        Intent it = new Intent(this, RvSimpleTestActivity.class);
+//        startActivity(it);
     }
 
     public void clickCardRv(View v){
-        Intent it = new Intent(this, RvCardsActivity.class);
-        startActivity(it);
+//        Intent it = new Intent(this, RvCardsActivity.class);
+//        startActivity(it);
     }
 
     public void clickSnackBar(View v){
@@ -68,5 +70,5 @@ public class MainActivity extends Activity {
         startActivity(it);
     }
 
-
 }
+
