037362e2c4b97043438fcbe47b76f8e2a94323ae
songzhw <songzhw2012@gmail.com>
2017-03-15 13:44:11 -0400

    rv: card stack

--- SupportDemo/app/src/main/java/cn/six/sup/rv/swipe_card/SwipeCardLayoutManager.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/swipe_card/SwipeCardLayoutManager.java
@@ -19,25 +19,27 @@ public class SwipeCardLayoutManager extends RecyclerView.LayoutManager {
 
     @Override // from LayoutManager
     public void onLayoutChildren(RecyclerView.Recycler rv, RecyclerView.State state) {
-        this.detachAndScrapAttachedViews(rv); // rv's method, detach all the children views
+        super.onLayoutChildren(rv, state);
+        removeAllViews();
+        detachAndScrapAttachedViews(rv); // rv's method, detach all the children views
 
-        int childCount = this.getChildCount();
+        int childCount = this.getItemCount();
         for (int i = 0; i < childCount; i++) {
             View child = rv.getViewForPosition(i);
+            addView(child);
 
             // measure child
             this.measureChildWithMargins(child, 0, 0); //rv's method. 后两参会参与width,height的计算
-            addView(child);
 
             // layout child
             int width = this.getDecoratedMeasuredWidth(child);
             int height = this.getDecoratedMeasuredHeight(child);
             // 每个child的left和top都一样，直接设置为0就可以了，这样child就依次叠加在一起了
-            this.layoutDecorated(child, 0, 0, width, height);
+            this.layoutDecoratedWithMargins(child, 0, 0, width, height);
 
             if(i < (childCount - 1)){
-                child.setScaleX(0.8f);
-                child.setScaleY(0.8f);
+                child.setScaleX(1.2f);
+                child.setScaleY(1.2f);
             }
         }
     }
