6783fc5e5981dada1319ec53840eca8a29a8d64d
songzhw <songzhw2012@gmail.com>
2017-04-04 19:58:07 -0400

    bug?: no value is passed to the target pages

--- SupportDemo/app/src/main/java/cn/six/sup/other/notification/Notification_A.java
+++ SupportDemo/app/src/main/java/cn/six/sup/other/notification/Notification_A.java
@@ -24,23 +24,31 @@ public class Notification_A extends Activity {
         tv.setText("Page AAA || "+getIntent().getStringExtra("key"));
     }
 
-    public void onClickSimpleButton(View v){}
+    public void onClickSimpleButton(View v){showNotification();}
     public void onClickSimpleButton2(View v){}
 
     private void showNotification(){
         Intent it1 = new Intent(this, Notification_B.class);
+        it1.putExtra("key","This is B");
         PendingIntent pi1 = PendingIntent.getActivity(this, 0, it1, 0);
 
         Intent it2 = new Intent(this, Notification_C.class);
+        it2.putExtra("key","This is C");
         PendingIntent pi2 = PendingIntent.getActivity(this, 0, it2, 0);
 
         Intent it3 = new Intent(this, Notification_D.class);
+        it3.putExtra("key","This is D");
         PendingIntent pi3 = PendingIntent.getActivity(this, 0, it3, 0);
+
+        Intent it4 = new Intent(this, Notification_E.class);
+        it4.putExtra("key","This is E");
+        PendingIntent pi4 = PendingIntent.getActivity(this, 0, it4, 0);
 
         Notification notification = new NotificationCompat.Builder(this)
                 .setContentTitle("Title")
                 .setContentText("Good Friday is coming")
                 .setSmallIcon(R.drawable.ic_pets)
+                .setContentIntent(pi4)
                 .setAutoCancel(true)
                 .addAction(R.drawable.ic_alarm, "Alarm", pi1)
                 .addAction(R.drawable.ic_notifications, "Notify", pi2)
