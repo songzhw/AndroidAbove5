b16885460f357aeef02286dac54c9461fbbc1098
songzhw <songzhw2012@gmail.com>
2017-04-27 21:05:24 -0400

    introduction of toolbar

--- AppCompatDemo/app/src/main/java/ca/six/demo/HomeActivity.java
+++ AppCompatDemo/app/src/main/java/ca/six/demo/HomeActivity.java
@@ -3,8 +3,10 @@ package ca.six.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
 
     private Toolbar toolbar;
 
@@ -18,5 +20,21 @@ public class HomeActivity extends AppCompatActivity {
         toolbar.setSubtitle("subtitle");
         toolbar.setLogo(R.drawable.ic_launcher_round);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_left);
+
+        setSupportActionBar(toolbar);
+        toolbar.setOnMenuItemClickListener(this);
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if(item.getItemId() == R.id.action_notifications){
+            System.out.println("szw notification");
+        }
+        return true;
     }
 }
