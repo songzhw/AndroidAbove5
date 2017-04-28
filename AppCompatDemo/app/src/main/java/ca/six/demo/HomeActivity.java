761f78e3d87517bca8a9353175c4a43154de9e0a
songzhw <songzhw2012@gmail.com>
2017-04-27 20:32:53 -0400

    introduction of toolbar

--- AppCompatDemo/app/src/main/java/ca/six/demo/HomeActivity.java
+++ AppCompatDemo/app/src/main/java/ca/six/demo/HomeActivity.java
@@ -2,12 +2,21 @@ package ca.six.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Title2");
+        toolbar.setSubtitle("subtitle");
+        toolbar.setLogo(R.drawable.ic_launcher_round);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_left);
     }
 }
