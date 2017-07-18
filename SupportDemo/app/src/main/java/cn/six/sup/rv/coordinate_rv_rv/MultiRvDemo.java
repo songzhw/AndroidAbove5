23da114ed74a3434ec010e6b591fb30ebde4d605
songzhw <songzhw2012@gmail.com>
2017-07-17 21:53:52 -0400

    bug: no coordinated affect between two rv

--- SupportDemo/app/src/main/java/cn/six/sup/rv/coordinate_rv_rv/MultiRvDemo.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/coordinate_rv_rv/MultiRvDemo.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.HorizontalScrollView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,9 @@ import cn.six.sup.rv.one_adapter.OneAdapter;
 public class MultiRvDemo extends Activity {
     public static final int HEIGHT = 15;
     public static final int WIDTH = 7;
+    private RecyclerView rvLeft;
+    private RecyclerView rvRight;
+    private HorizontalScrollView hsv;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,8 +38,7 @@ public class MultiRvDemo extends Activity {
             dataRight.add(""+i);
         }
 
-
-        RecyclerView rvLeft = (RecyclerView)findViewById(R.id.rvMultiRvLeft);
+        rvLeft = (RecyclerView)findViewById(R.id.rvMultiRvLeft);
         rvLeft.setLayoutManager(new LinearLayoutManager(this));
         rvLeft.setAdapter(new OneAdapter<String>(R.layout.item_left, dataLeft) {
             @Override
@@ -43,8 +46,9 @@ public class MultiRvDemo extends Activity {
                 vh.setText(R.id.tvItemSymbol, s);
             }
         });
+        initRvLeftScrollListener();
 
-        RecyclerView rvRight = (RecyclerView)findViewById(R.id.rvMultiRvRight);
+        rvRight = (RecyclerView)findViewById(R.id.rvMultiRvRight);
         rvRight.setLayoutManager(new GridLayoutManager(this, WIDTH));
         rvRight.setAdapter(new OneAdapter<String>(R.layout.item_right, dataRight) {
             @Override
@@ -52,7 +56,74 @@ public class MultiRvDemo extends Activity {
                 vh.setText(R.id.tvItemDetails, s);
             }
         });
+        initRvRightScrollListener();
+
+        hsv = (HorizontalScrollView)findViewById(R.id.hsvRight);
 
     }
+
+    private final RecyclerView.OnScrollListener rvLeftScrollListener = new MultiRvScrollListener(){
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy); // 当我滑动完了， 成idle了， rvLeft移除此监听
+            rvRight.scrollBy(dx, dy); // rvLeft的移动， 要求rvRight也做相关动作。
+        }
+    };
+
+    private final RecyclerView.OnScrollListener rvRightScrollListener = new MultiRvScrollListener() {
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy); // 当我滑动完了， 成idle了， rvLeft移除此监听
+            rvLeft.scrollBy(dx, dy); //rvRight的移动，也要求rvLeft动。 这和上面一比对，明显会无限循环。 所以要做处理。
+        }
+    };
+
+    private void initRvLeftScrollListener(){
+        rvLeft.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            int lastY;
+
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+                int action = e.getAction();
+                // action_down保证只addOnScrollListener()一次，不会添加多个listener
+                // rvRight是idle， 保证rvRight没有onScrollListener，这样就不会无限死循环
+                if(action == MotionEvent.ACTION_DOWN && rvRight.getScrollState() == RecyclerView.SCROLL_STATE_IDLE){
+                    lastY = rv.getScrollY();
+                    rv.addOnScrollListener(rvLeftScrollListener);
+                }
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) { }
+        });
+    }
+
+    private void initRvRightScrollListener(){
+        rvRight.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            int lastY;
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+                int action = e.getAction();
+                if(action == MotionEvent.ACTION_DOWN && rvLeft.getScrollState() == RecyclerView.SCROLL_STATE_IDLE) {
+                    lastY = rv.getScrollY();
+                    rv.addOnScrollListener(rvRightScrollListener);
+                }
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) { }
+        });
+    }
 }
+
 
