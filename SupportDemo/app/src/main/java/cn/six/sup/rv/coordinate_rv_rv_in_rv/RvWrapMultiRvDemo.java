3e4f65160ec3bd8a60eda1d0424189aab998b57b
songzhw <songzhw2012@gmail.com>
2017-07-18 21:54:42 -0400

    add demo for one adapter for multiple types

---
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/coordinate_rv_rv_in_rv/RvWrapMultiRvDemo.java
@@ -0,0 +1,40 @@
+package cn.six.sup.rv.coordinate_rv_rv_in_rv;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.six.sup.R;
+import cn.six.sup.rv.ItemView;
+import cn.six.sup.rv.OneTypesAdapter;
+
+public class RvWrapMultiRvDemo extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actv_rv_wrap_multi_rv);
+
+        RecyclerView rv = (RecyclerView)findViewById(R.id.rvWrapMultiRv);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+        List<ItemView> data = new ArrayList<>();
+        data.add(new SimpleMenuRow());
+        data.add(new SimpleMenuRow());
+        data.add(new SimpleMenuRow());
+        data.add(new SimpleMenuRow());
+        data.add(new SimpleMenuRow());
+        data.add(new SimpleMenuRow());
+        data.add(new SimpleMenuRow());
+        rv.setAdapter(new OneTypesAdapter(data));
+
+    }
+
+}
+
---
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/coordinate_rv_rv_in_rv/SimpleMenuRow.java
@@ -0,0 +1,18 @@
+package cn.six.sup.rv.coordinate_rv_rv_in_rv;
+
+
+import cn.six.sup.R;
+import cn.six.sup.rv.ItemView;
+import cn.six.sup.rv.RvViewHolder;
+
+public class SimpleMenuRow implements ItemView {
+    @Override
+    public int getViewType() {
+        return R.layout.item_menu;
+    }
+
+    @Override
+    public void bind(RvViewHolder holder) {
+        holder.setText(R.id.tvItemMenu, "id = "+ Math.random() * 10000);
+    }
+}
