d5306e8136ad6fffba6e7bf32074e1b7740d64d1
songzhw <songzhw2012@gmail.com>
2017-08-06 08:09:10 -0400

    still failed

--- SupportDemo/app/src/main/java/cn/six/sup/other/spring/SpringActivity01.java
+++ SupportDemo/app/src/main/java/cn/six/sup/other/spring/SpringActivity01.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.animation.SpringAnimation;
 import android.support.animation.SpringForce;
 import android.support.annotation.Nullable;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 
 import cn.six.sup.R;
@@ -17,11 +19,22 @@ public class SpringActivity01 extends Activity {
         setContentView(R.layout.activity_spring_one);
         ImageView iv = (ImageView) findViewById(R.id.ivSpring01);
 
-        SpringForce force = new SpringForce(0f)
-                .setDampingRatio(SpringForce.DAMPING_RATIO_LOW_BOUNCY)
-                .setStiffness(SpringForce.STIFFNESS_MEDIUM);
-        SpringAnimation anim = new SpringAnimation(iv, SpringAnimation.TRANSLATION_Y)
+        final SpringForce force = new SpringForce(0f)
+                .setDampingRatio(SpringForce.DAMPING_RATIO_LOW_BOUNCY) // 阻力。 振幅会逐渐下降。
+                .setStiffness(SpringForce.STIFFNESS_MEDIUM); // 刚度。 多久回到原点。
+        final SpringAnimation anim = new SpringAnimation(iv, SpringAnimation.TRANSLATION_Y)
                 .setSpring(force);
-        anim.start();
+
+        iv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    anim.start();
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
 }
