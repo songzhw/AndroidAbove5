bb840315e05aa863920c84d318ed2530d4df805d
songzhw <songzhw2012@gmail.com>
2017-08-09 21:29:51 -0400

    update the spring animation demo 03, which is more easier now

--- SupportDemo/app/src/main/java/cn/six/sup/other/spring/SpringActivity03.java
+++ SupportDemo/app/src/main/java/cn/six/sup/other/spring/SpringActivity03.java
@@ -1,7 +1,5 @@
 package cn.six.sup.other.spring;
 
-import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.animation.DynamicAnimation;
@@ -9,18 +7,9 @@ import android.support.animation.SpringAnimation;
 import android.support.animation.SpringForce;
 import android.support.annotation.FloatRange;
 import android.support.annotation.Nullable;
-import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import java.util.ArrayList;
 
 import cn.six.sup.R;
 
@@ -33,18 +22,8 @@ public class SpringActivity03 extends Activity {
 
         movingView = findViewById(R.id.movingView);
 
-        // 以图片的初始位置创建动画对象
-        movingView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                xAnimation = createSpringAnimation(
-                        movingView, SpringAnimation.X, movingView.getX(), STIFFNESS, DAMPING_RATIO);
-                yAnimation = createSpringAnimation(
-                        movingView, SpringAnimation.Y, movingView.getY(), STIFFNESS, DAMPING_RATIO);
-                //初始位置确定，移除监听
-                movingView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-            }
-        });
+        xAnimation = createSpringAnimation(movingView, SpringAnimation.TRANSLATION_X, 0);
+        yAnimation = createSpringAnimation(movingView, SpringAnimation.TRANSLATION_Y, 0);
 
         movingView.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -78,27 +57,13 @@ public class SpringActivity03 extends Activity {
 
     }
 
-    /**
-     * 创建弹性动画
-     *
-     * @param view          动画关联的控件
-     * @param property      动画作用的属性
-     * @param finalPosition 动画结束的位置
-     * @param stiffness     硬度
-     * @param dampingRatio  阻尼
-     * @return
-     */
-    SpringAnimation createSpringAnimation(View view,
-                                          DynamicAnimation.ViewProperty property,
-                                          Float finalPosition,
-                                          @FloatRange(from = 0.0) Float stiffness,
-                                          @FloatRange(from = 0.0) Float dampingRatio) {
+    SpringAnimation createSpringAnimation(View view, DynamicAnimation.ViewProperty property, float finalPosition) {
         //创建弹性动画类SpringAnimation
         SpringAnimation animation = new SpringAnimation(view, property);
         //SpringForce类，定义弹性特质
         SpringForce spring = new SpringForce(finalPosition);
-        spring.setStiffness(stiffness);
-        spring.setDampingRatio(dampingRatio);
+        spring.setStiffness(STIFFNESS);
+        spring.setDampingRatio(DAMPING_RATIO);
         //关联弹性特质
         animation.setSpring(spring);
         return animation;
@@ -117,3 +82,37 @@ public class SpringActivity03 extends Activity {
     float dY = 0f;
 
 }
+
+/*
+        // 以图片的初始位置创建动画对象
+        movingView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                xAnimation = createSpringAnimation(
+                        movingView, SpringAnimation.X, movingView.getX(), STIFFNESS, DAMPING_RATIO);
+                yAnimation = createSpringAnimation(
+                        movingView, SpringAnimation.Y, movingView.getY(), STIFFNESS, DAMPING_RATIO);
+                //初始位置确定，移除监听
+                movingView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+*/
+
+
+/*
+    SpringAnimation createSpringAnimation(View view,
+                                               DynamicAnimation.ViewProperty property,
+                                               Float finalPosition,
+                                               @FloatRange(from = 0.0) Float stiffness,
+                                               @FloatRange(from = 0.0) Float dampingRatio) {
+        //创建弹性动画类SpringAnimation
+        SpringAnimation animation = new SpringAnimation(view, property);
+        //SpringForce类，定义弹性特质
+        SpringForce spring = new SpringForce(finalPosition);
+        spring.setStiffness(stiffness);
+        spring.setDampingRatio(dampingRatio);
+        //关联弹性特质
+        animation.setSpring(spring);
+        return animation;
+    }
+*/
