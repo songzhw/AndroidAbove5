ad27fef781a8ca361ebd30b040c24c2f56945483
songzhw <songzhw2012@gmail.com>
2017-08-18 19:21:11 -0400

    Data Diff Utils (but the result is failed)

--- SupportDemo/app/src/main/java/cn/six/sup/rv/diff/DataDiffDemo.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/diff/DataDiffDemo.java
@@ -3,8 +3,10 @@ package cn.six.sup.rv.diff;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 
 import java.util.ArrayList;
@@ -14,7 +16,10 @@ import cn.six.sup.R;
 import cn.six.sup.rv.RvViewHolder;
 import cn.six.sup.rv.one_adapter.OneAdapter;
 
-public class DataDiffDemo extends Activity {
+public class DataDiffDemo extends Activity implements View.OnClickListener {
+
+    private OneAdapter<String> adapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +33,29 @@ public class DataDiffDemo extends Activity {
             data.add("String Index " + i);
         }
 
-        OneAdapter<String> adapter = new OneAdapter<String>(R.layout.item_single_rv, data) {
+        adapter = new OneAdapter<String>(R.layout.item_single_rv, data) {
             @Override
             protected void apply(RvViewHolder vh, String s, int position) {
                 vh.setText(R.id.tvSingleRvItem, "xx => " + s);
             }
         };
         rv.setAdapter(adapter);
+
+        findViewById(R.id.btnSingleRv).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        List<String> olds = adapter.data;
+
+        int size = (int)(Math.random() * 50);
+        List<String> news = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+//            news.add("(" + size + ") new size " + i);
+            news.add("String Index " + i);
+        }
+
+        DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(new DemoDataDiff(olds, news));
+        diffResult.dispatchUpdatesTo(adapter);
     }
 }
---
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/diff/DemoDataDiff.java
@@ -0,0 +1,35 @@
+package cn.six.sup.rv.diff;
+
+import android.support.v7.util.DiffUtil;
+
+import java.util.List;
+
+public class DemoDataDiff extends DiffUtil.Callback {
+    private List<String> oldData, newData;
+
+    public DemoDataDiff(List<String> oldData, List<String> newData) {
+        this.oldData = oldData;
+        this.newData = newData;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return oldData == null ? 0 : oldData.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return newData == null ? 0 : newData.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldData.get(oldItemPosition).getClass().equals(
+            newData.get(newItemPosition).getClass());
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldData.get(oldItemPosition).equals(newData.get(newItemPosition));
+    }
+}
