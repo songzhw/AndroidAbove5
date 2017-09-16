c3ce3088334068c70a4beed3db72da19437fd051
songzhw <songzhw2012@gmail.com>
2017-09-16 00:35:43 -0400

    lifecycle order issue

--- SupportDemo/app/src/main/java/cn/six/sup/g_component/lifecycle/LifeWatcherDemo01.java
+++ SupportDemo/app/src/main/java/cn/six/sup/g_component/lifecycle/LifeWatcherDemo01.java
@@ -8,23 +8,42 @@ import android.view.View;
 
 import cn.six.sup.R;
 
-
 public class LifeWatcherDemo01 extends LifecycleActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        System.out.println("szw onCreate 01");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tv_btn);
 
         Watcher01 watcher01 = new Watcher01();
         getLifecycle().addObserver(watcher01);
+        System.out.println("szw onCreate 01");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("szw onResume 01");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("szw onPause 01");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("szw onDestroy 01");
     }
 
     public void onClickSimpleButton(View v) {
-        startActivity(new Intent(this, LifeWatcherDemo01.class));
     }
 
     public void onClickSimpleButton2(View v) {
     }
 }
+
 
