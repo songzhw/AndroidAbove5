5f25bac1af421867387c7e9821b70cbd82bf1994
songzhw <songzhw2012@gmail.com>
2017-09-17 00:15:16 -0400

    refactor StickyColumnTableView

--- SupportDemo/app/src/main/java/cn/six/sup/rv/cooridinate_rv_rv_2/StickyColumnTableView.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/cooridinate_rv_rv_2/StickyColumnTableView.java
@@ -1,4 +1,4 @@
-package cn.six.sup.rv.coordinate_rv_rv.demo2;
+package cn.six.sup.rv.cooridinate_rv_rv_2;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
@@ -13,8 +13,6 @@ import android.widget.LinearLayout;
 
 import cn.six.sup.R;
 import cn.six.sup.rv.RvViewHolder;
-import cn.six.sup.rv.cooridinate_rv_rv_2.MultiRvScrollListener;
-import cn.six.sup.rv.cooridinate_rv_rv_2.ObservableHorizontalScrollView;
 import cn.six.sup.rv.one_adapter.OneAdapter;
 
 /**
--- SupportDemo/app/src/main/java/cn/six/sup/rv/cooridinate_rv_rv_2/demo/MultiRvDemo2.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/cooridinate_rv_rv_2/demo/MultiRvDemo2.java
@@ -3,133 +3,39 @@ package cn.six.sup.rv.cooridinate_rv_rv_2.demo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.MotionEvent;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import cn.six.sup.R;
-import cn.six.sup.rv.RvViewHolder;
-import cn.six.sup.rv.cooridinate_rv_rv_2.MultiRvScrollListener;
-import cn.six.sup.rv.cooridinate_rv_rv_2.ObservableHorizontalScrollView;
-import cn.six.sup.rv.one_adapter.OneAdapter;
+import cn.six.sup.rv.cooridinate_rv_rv_2.StickyColumnTableAdapter;
+import cn.six.sup.rv.cooridinate_rv_rv_2.StickyColumnTableView;
 
 public class MultiRvDemo2 extends Activity {
     public static final int HEIGHT = 15;
     public static final int WIDTH = 7;
-    private RecyclerView rvLeft;
-    private RecyclerView rvRight;
-    private ObservableHorizontalScrollView hsv;
-    private CoordinateRvScrollListener rvLeftScrollListener, rvRightScrollListener;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.actv_multi_rv);
+        setContentView(R.layout.actv_multi_rv_two);
 
         List<String> dataLeft = new ArrayList<>();
-        for(int i = 1; i <= HEIGHT; i++){
-            dataLeft.add(""+i);
+        for (int i = 1; i <= HEIGHT; i++) {
+            dataLeft.add("" + i);
         }
 
         List<String> dataRight = new ArrayList<>();
         int sum = HEIGHT * WIDTH;
-        for(int i = 1; i <= sum; i++){
-            dataRight.add(""+i);
+        for (int i = 1; i <= sum; i++) {
+            dataRight.add("" + i);
         }
 
-        rvLeft = (RecyclerView)findViewById(R.id.rvMultiRvLeft);
-        rvLeft.setLayoutManager(new LinearLayoutManager(this));
-        rvLeft.setAdapter(new OneAdapter<String>(R.layout.item_left, dataLeft) {
-            @Override
-            protected void apply(RvViewHolder vh, String s, int position) {
-                vh.setText(R.id.tvItemSymbol, s);
-            }
-        });
-
-        rvRight = (RecyclerView)findViewById(R.id.rvMultiRvRight);
-        rvRight.setLayoutManager(new GridLayoutManager(this, WIDTH));
-        rvRight.setAdapter(new OneAdapter<String>(R.layout.item_right, dataRight) {
-            @Override
-            protected void apply(RvViewHolder vh, String s, int position) {
-                vh.setText(R.id.tvItemDetails, s);
-            }
-        });
-
-        rvLeftScrollListener = new CoordinateRvScrollListener(rvRight);
-        rvLeft.addOnItemTouchListener(new CoordinateRvItemTouchListener(rvRight, rvLeftScrollListener));
-
-        rvRightScrollListener = new CoordinateRvScrollListener(rvLeft);
-        rvRight.addOnItemTouchListener(new CoordinateRvItemTouchListener(rvLeft, rvRightScrollListener));
-
-        hsv = (ObservableHorizontalScrollView)findViewById(R.id.hsvRight);
-        hsv.setScrollViewListener(new ObservableHorizontalScrollView.ScrollViewListener() {
-            @Override
-            public void onScrollChanged(ObservableHorizontalScrollView scrollView, int x, int y, int oldx, int oldy) {
-                rvLeft.removeOnScrollListener(rvLeftScrollListener);
-                rvRight.removeOnScrollListener(rvRightScrollListener);
-            }
-        });
+        StickyColumnTableAdapter<String> adapter = new StickyColumnTableAdapter<>(dataLeft, dataRight);
+        StickyColumnTableView tableView = (StickyColumnTableView) findViewById(R.id.sctv_demo2);
+        tableView.setAdapter(adapter);
+        tableView.refresh();
     }
 
-    // 当我滑动完了， 成idle了， recyclerView会移除此监听
-    private class CoordinateRvScrollListener extends MultiRvScrollListener {
-        private RecyclerView rvOther;
-
-        public CoordinateRvScrollListener(RecyclerView rvOther) {
-            this.rvOther = rvOther;
-        }
-
-        @Override
-        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-            super.onScrolled(recyclerView, dx, dy);
-            rvOther.scrollBy(dx, dy);
-        }
-    }
-
-
-    // 在rvOther是idle时， 才会真的能移动
-    private class CoordinateRvItemTouchListener implements RecyclerView.OnItemTouchListener {
-        private RecyclerView rvOther;
-        private CoordinateRvScrollListener scrollListener;
-        private int lastY;
-
-        public CoordinateRvItemTouchListener(RecyclerView rvOther,
-                                             CoordinateRvScrollListener scrollListener) {
-            this.rvOther = rvOther;
-            this.scrollListener = scrollListener;
-        }
-
-        @Override
-        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-            // fire the touch event, otherwise ,the onTouchEvent() will never get called
-            if(rv.getScrollState() == RecyclerView.SCROLL_STATE_IDLE) {
-                onTouchEvent(rv, e);
-            }
-            return false;
-        }
-
-        @Override
-        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
-            int action = e.getAction();
-            if(action == MotionEvent.ACTION_DOWN && rvOther.getScrollState() == RecyclerView.SCROLL_STATE_IDLE){
-                lastY = rv.getScrollY();
-                rv.addOnScrollListener(scrollListener);
-            } else {
-                // if this touch is not a scrolling action, remove the scroll listener
-                if(action == MotionEvent.ACTION_UP && rv.getScrollY() == lastY) {
-                    rv.removeOnScrollListener(scrollListener);
-                }
-            }
-        }
-
-        @Override
-        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) { }
-    }
 }
-
-
 
