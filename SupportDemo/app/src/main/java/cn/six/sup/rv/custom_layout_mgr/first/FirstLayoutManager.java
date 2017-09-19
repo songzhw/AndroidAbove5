dd26e69f38fe0afb1568aa750e327b2d7a9b6491
songzhw <songzhw2012@gmail.com>
2017-09-18 20:55:38 -0400

    vertical layout

--- SupportDemo/app/src/main/java/cn/six/sup/rv/custom_layout_mgr/first/FirstLayoutManager.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/custom_layout_mgr/first/FirstLayoutManager.java
@@ -52,32 +52,19 @@ class FirstLayoutManager extends RecyclerView.LayoutManager {
         return true;
     }
 
-    // 看rv下面的内容, 即往上拉, dy就是正值; 否则dy就是负值. (dy相当于move, 每move一次就有一次dy值.)
+    // 1. 看rv下面的内容, 即往上拉, dy就是正值; 否则dy就是负值. (dy相当于move, 每move一次就有一次dy值.)
     // 注意, 到底后,再往上拉, dy仍是有的, 仍是正值. 值的大小看你的拉动的距离有多大.
+    // 2. 到底部: rv是match_parent的, 数据超过了一屏. 这时totalHeight = 3744, getVerticalSpace = 2308, 二值差为1436
+    // 这时的verticalScrollOffset最大的值, 就是这个1436
     @Override
     public int scrollVerticallyBy(int dy, RecyclerView.Recycler recycler, RecyclerView.State state) {
-        int travel = dy; //实际要滑动的距离
-        int verticalSpace = getVerticalSpace();
-        System.out.println(" szw scrollVerticallyBy() : verticalScrollOffset = "+verticalScrollOffset);
-
-        // 如果滑到了最顶部
-        if (verticalScrollOffset + dy < 0) {
-            travel = -verticalScrollOffset;
-        }
-        // 如果滑到最底部
-        else if (verticalScrollOffset + dy > (totalHeight - verticalSpace)) {
-            travel = totalHeight - verticalSpace - verticalScrollOffset;
-        }
-
-        verticalScrollOffset += travel;
-        offsetChildrenVertical(-travel);
-        return travel;
+        offsetChildrenVertical(-dy);
+        return dy;
     }
 
     // 获取RecyclerView在垂直方向上的可用空间，即去除了padding后的高度
     private int getVerticalSpace() {
         return getHeight() - getPaddingBottom() - getPaddingTop(); //此三参数, 皆是来自rv
     }
-
 
 }
