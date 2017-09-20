623247aa080ef0a62693b9b39d6ea131493ec6be
songzhw <songzhw2012@gmail.com>
2017-09-20 15:30:08 -0400

    HexLayout

---
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/custom_layout_mgr/hex/MathUtil.java
@@ -0,0 +1,14 @@
+package cn.six.sup.rv.custom_layout_mgr.hex;
+
+public class MathUtil {
+
+    public static float sin(int angle) {
+        return (float) Math.sin(angle * Math.PI / 180);
+    }
+
+    public static float cos(int angle) {
+        return (float) Math.cos(angle * Math.PI / 180);
+    }
+
+
+}
---
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/custom_layout_mgr/hex/demo/HexViewDemo.java
@@ -0,0 +1,19 @@
+package cn.six.sup.rv.custom_layout_mgr.hex.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import cn.six.sup.R;
+
+
+public class HexViewDemo extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hex_view_demo);
+
+
+    }
+}
