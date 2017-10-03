835efcea22dfd9f9df0f4526100f749491256698
songzhw <songzhw2012@gmail.com>
2017-10-02 20:28:13 -0400

    add sample

--- SupportDemo/app/src/main/java/cn/six/sup/rv/custom_layout_mgr/fixed_column/demo/FixColumnGridDemo.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/custom_layout_mgr/fixed_column/demo/FixColumnGridDemo.java
@@ -6,7 +6,14 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.six.sup.R;
+import cn.six.sup.rv.custom_layout_mgr.fixed_column.demo.entity.Action;
+import cn.six.sup.rv.custom_layout_mgr.fixed_column.demo.entity.IFixedGridType;
+import cn.six.sup.rv.custom_layout_mgr.fixed_column.demo.entity.Name;
+import cn.six.sup.rv.custom_layout_mgr.fixed_column.demo.entity.Numbera;
 
 public class FixColumnGridDemo extends Activity {
 
@@ -19,12 +26,26 @@ public class FixColumnGridDemo extends Activity {
         rv.setHasFixedSize(true);
         rv.setLayoutManager(new GridLayoutManager(this, 6));
 
+        List<IFixedGridType> data = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            Name name = new Name();
+            name.name = "Item " + i;
+            name.subName = "description " + i;
+            data.add(name);
 
+            for (int j = 0; j < 20; j++) {
+                Numbera num = new Numbera();
+                num.num = i * 30 + j;
+                data.add(num);
+            }
 
-
+            Action action = new Action();
+            data.add(action);
+        }
         GridDemoAdapter adapter = new GridDemoAdapter();
-
+        adapter.data = data;
         rv.setAdapter(adapter);
+
     }
 
 
