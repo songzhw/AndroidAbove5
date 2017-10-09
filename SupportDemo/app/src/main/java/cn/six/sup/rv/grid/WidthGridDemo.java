1a41acd9e8e2126d97f37b19ef1c8342b57bf2df
songzhw <songzhw2012@gmail.com>
2017-10-08 21:32:10 -0400

    grid layout manager : same width?

--- SupportDemo/app/src/main/java/cn/six/sup/rv/grid/WidthGridDemo.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/grid/WidthGridDemo.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -22,15 +23,15 @@ public class WidthGridDemo extends Activity {
         setContentView(R.layout.activity_rv_demo_wrap);
 
         rv = (RecyclerView) findViewById(R.id.rvRefresh);
-        rv.setLayoutManager(new GridLayoutManager(this, 4));
+        rv.setLayoutManager(new GridLayoutManager(this, 4, LinearLayoutManager.HORIZONTAL, false));
 
         List<IWidthType> data = new ArrayList<>();
         for(int i = 0; i < 20; i++) {
             data.add(new WidthNumber(""+ (100+i)));
             data.add(new WidthNumber(""+ (200+i)));
             data.add(new WidthNumber(""+ (300+i)));
-            data.add(new WidthNumber(""+ (400+i)));
-//            data.add(new WidthAction());
+//            data.add(new WidthNumber(""+ (400+i)));
+            data.add(new WidthAction());
         }
 
         WidthGridAdapter adapter = new WidthGridAdapter();
