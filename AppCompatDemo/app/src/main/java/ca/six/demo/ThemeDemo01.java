950a2bcc7ebee61338008554b27049803b8cee14
songzhw <songzhw2012@gmail.com>
2017-10-11 20:33:33 -0400

    appcompat demo: bg is invalid?!

---
+++ AppCompatDemo/app/src/main/java/ca/six/demo/ThemeDemo01.java
@@ -0,0 +1,15 @@
+package ca.six.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class ThemeDemo01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theme_one);
+    }
+}
--- SupportDemo/app/src/main/java/cn/six/sup/rv/grid/WidthGridDemo.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/grid/WidthGridDemo.java
@@ -1,10 +1,13 @@
 package cn.six.sup.rv.grid;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
