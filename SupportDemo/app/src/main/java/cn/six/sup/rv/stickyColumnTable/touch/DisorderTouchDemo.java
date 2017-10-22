4c8023b6aa5df003b549e84395268bbdc260dbf1
songzhw <songzhw2012@gmail.com>
2017-10-21 21:57:04 -0400

    touch issue in complex page

--- SupportDemo/app/src/main/java/cn/six/sup/rv/stickyColumnTable/touch/DisorderTouchDemo.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/stickyColumnTable/touch/DisorderTouchDemo.java
@@ -4,12 +4,23 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.six.sup.R;
+import cn.six.sup.rv.composition.BaseComposedAdapter;
+import cn.six.sup.rv.composition.BaseRow;
+import cn.six.sup.rv.composition.ClothRow;
+import cn.six.sup.rv.composition.demo.TwoTextRow;
 
 public class DisorderTouchDemo extends AppCompatActivity {
+
+    private RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +37,24 @@ public class DisorderTouchDemo extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        rv = (RecyclerView) findViewById(R.id.rvRefresh);
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+        List<BaseRow> rows = new ArrayList<>();
+        rows.add(new TwoTextRow("sing1","song1"));
+        rows.add(new TwoTextRow("sing2","song2"));
+        rows.add(new TwoTextRow("sing3","song3"));
+        rows.add(new TwoTextRow("sing4","song4"));
+        rows.add(new TwoTextRow("sing5","song5"));
+        rows.add(new TwoTextRow("sing6","song6"));
+        rows.add(new TwoTextRow("sing7","song7"));
+        rows.add(new TwoTextRow("sing8","song8"));
+        rows.add(new TwoTextRow("sing9","song9"));
+        rows.add(new ClothRow("leisi","lei si", "Buy", null));
+        rows.add(new ClothRow("trespassing","Test definition, the means by which the presence, quality, or genuineness of anything is determined; a means of trial", "Look", null));
+        BaseComposedAdapter adapter = new BaseComposedAdapter(rows);
+        rv.setAdapter(adapter);
     }
 }
