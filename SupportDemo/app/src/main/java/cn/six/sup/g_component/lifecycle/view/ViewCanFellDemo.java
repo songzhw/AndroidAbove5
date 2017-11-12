26d0aed462844cc698de15bb5a32752abd163cd3
songzhw <songzhw2012@gmail.com>
2017-11-12 00:29:36 -0500

    view can sense the lifecycle of activity

--- SupportDemo/app/src/main/java/cn/six/sup/g_component/lifecycle/view/ViewCanFellDemo.java
+++ SupportDemo/app/src/main/java/cn/six/sup/g_component/lifecycle/view/ViewCanFellDemo.java
@@ -1,20 +1,20 @@
 package cn.six.sup.g_component.lifecycle.view;
 
-import android.app.Activity;
-import android.arch.lifecycle.LifecycleActivity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import cn.six.sup.g_component.LifeAppCompatActivity;
 
-public class ViewCanFellDemo extends LifecycleActivity {
+
+public class ViewCanFellDemo extends LifeAppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         MyCustomView view = new MyCustomView(this);
-        setContentView(view);
-
         getLifecycle().addObserver(view);
+        setContentView(view);
     }
+
 }
