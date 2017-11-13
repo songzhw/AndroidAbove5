50033c146395dbf0b7e2c1e4dd7e1b54323c6860
songzhw <songzhw2012@gmail.com>
2017-11-13 11:14:07 -0500

    update the lifecycle demo

--- SupportDemo/app/src/main/java/cn/six/sup/g_component/lifecycle/view/ViewCanFellDemo.java
+++ SupportDemo/app/src/main/java/cn/six/sup/g_component/lifecycle/view/ViewCanFellDemo.java
@@ -2,11 +2,12 @@ package cn.six.sup.g_component.lifecycle.view;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 import cn.six.sup.g_component.LifeAppCompatActivity;
 
 
-public class ViewCanFellDemo extends LifeAppCompatActivity {
+public class ViewCanFellDemo extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
