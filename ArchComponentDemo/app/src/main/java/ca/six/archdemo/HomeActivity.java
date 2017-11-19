fc91987702aa582301f71f5dcb8065216be0ff94
songzhw <songzhw2012@gmail.com>
2017-11-18 20:25:54 -0500

    Architecture Component Library Demo

---
+++ ArchComponentDemo/app/src/main/java/ca/six/archdemo/HomeActivity.java
@@ -0,0 +1,15 @@
+package ca.six.archdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+
+}
