b6324e99d030672b7fdb5250834366fdf9f701ca
songzhw <songzhw2012@gmail.com>
2017-11-18 21:35:45 -0500

    Same class -- different instance

--- ArchComponentDemo/app/src/main/java/ca/six/archdemo/intro/same/SameClass02.java
+++ ArchComponentDemo/app/src/main/java/ca/six/archdemo/intro/same/SameClass02.java
@@ -1,13 +1,18 @@
-package ca.six.archdemo;
+package ca.six.archdemo.intro.same;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import ca.six.archdemo.R;
 
-public class BaseTvBtnActivity extends Activity {
+/**
+ * Created by songzhw on 2017/11/18.
+ */
+
+public class SameClass02 extends AppCompatActivity {
     private TextView tv;
 
     @Override
@@ -16,11 +21,13 @@ public class BaseTvBtnActivity extends Activity {
         setContentView(R.layout.activity_tv_btn);
         tv = findViewById(R.id.tv_simple);
 
+
     }
 
     public void onClickSimpleButton(View v) {
 
     }
+
 
     public void onClickSimpleButton2(View v) {
 
