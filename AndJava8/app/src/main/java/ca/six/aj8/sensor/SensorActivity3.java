2752efbf62242cd9d407f8e45479a02729c8cb7d
songzhw <songzhw2012@gmail.com>
2017-11-22 20:42:32 -0500

    sensor

--- AndJava8/app/src/main/java/ca/six/aj8/sensor/SensorActivity3.java
+++ AndJava8/app/src/main/java/ca/six/aj8/sensor/SensorActivity3.java
@@ -9,11 +9,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import ca.six.aj8.R;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.subjects.BehaviorSubject;
 
-public class SensorActivity2 extends AppCompatActivity implements SensorEventListener {
+public class SensorActivity3 extends AppCompatActivity implements SensorEventListener {
+    // Subject is a special type that can act both as an Observer and an Observable
     private BehaviorSubject<SensorEvent> proxy = BehaviorSubject.create();
     private SensorManager manager;
     private TextView tv;
@@ -23,24 +27,33 @@ public class SensorActivity2 extends AppCompatActivity implements SensorEventLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tv_btn);
         tv = findViewById(R.id.tv_simple);
-        manager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        manager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
-        proxy.subscribe( (sensorEvent) -> {
-            String result = sensorEvent.sensor.toString() + " \n " + Arrays.toString(sensorEvent.values);
-            tv.setText(result);
-        });
+        /*
+        The interval() method givs us an accurate timer and ensures the consistent time frame between the values.
+         */
+        Observable.interval(200, TimeUnit.MILLISECONDS)
+                .map((time) -> proxy.getValue()) //即每0.2秒就取一次proxy中的值. proxy再也不是一发就收了.
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe((sensorEvent -> {
+                    String result = sensorEvent.sensor.toString() + " \n " + Arrays.toString(sensorEvent.values);
+                    tv.setText(result);
+                    System.out.println("szw result = " + result);
+                }));
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         manager.registerListener(this,
-            manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), 100);
+                manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), 100);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        System.err.println("szw onPause()");
         manager.unregisterListener(this);
     }
 
