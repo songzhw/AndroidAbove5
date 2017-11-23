6d5ecc9cbda2a605a982374c786dbdec81fca088
songzhw <songzhw2012@gmail.com>
2017-11-22 20:43:51 -0500

    sensor

--- AndJava8/app/src/main/java/ca/six/aj8/sensor/SensorActivity4.java
+++ AndJava8/app/src/main/java/ca/six/aj8/sensor/SensorActivity4.java
@@ -90,3 +90,25 @@ public class SensorActivity4 extends AppCompatActivity implements SensorEventLis
     public void onAccuracyChanged(Sensor sensor, int i) {
     }
 }
+
+/*
+[How to stop Observable.interval()]
+:
+    private AtomicLong lastTick = new AtomicLong(0L);
+    private Subscription subscription;
+
+    void resume() {
+        System.out.println("resumed");
+        subscription = Observable.interval(5, TimeUnit.SECONDS, Schedulers.io())
+                                 .map(tick -> lastTick.getAndIncrement())
+                                 .subscribe(tick -> System.out.println("tick = " + tick));
+    }
+
+    void stop() {
+        if (subscription != null && !subscription.isUnsubscribed()) {
+            System.out.println("stopped");
+            subscription.unsubscribe();
+        }
+    }
+
+ */
