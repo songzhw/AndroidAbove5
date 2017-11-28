7b4568be9f9f08bb9ef40f5461688101fc63b4b3
songzhw <songzhw2012@gmail.com>
2017-11-27 22:47:01 -0500

    Architecture Component Library Demo : ViewModelFactory

--- ArchComponentDemo/app/src/main/java/ca/six/archdemo/intro/vmfactotry/DemoVmFactory.java
+++ ArchComponentDemo/app/src/main/java/ca/six/archdemo/intro/vmfactotry/DemoVmFactory.java
@@ -1,8 +1,12 @@
 package ca.six.archdemo.intro.vmfactotry;
 
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
 import android.support.annotation.NonNull;
+
+import java.lang.reflect.InvocationTargetException;
 
 
 public class DemoVmFactory extends ViewModelProvider.NewInstanceFactory {
@@ -10,6 +14,11 @@ public class DemoVmFactory extends ViewModelProvider.NewInstanceFactory {
     @NonNull
     @Override
     public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if(modelClass.isAssignableFrom(DemoVm.class)){
+            System.out.println("szw Factory if(demoVm)");
+            return (T) new DemoVm(23);  //这个(T)是为了不让你再强转了. 就像新版本的findViewById一样.
+        }
+        System.out.println("szw Factory else");
         return super.create(modelClass);
     }
 }
@@ -32,5 +41,43 @@ public static class NewInstanceFactory implements ViewModelProvider.Factory {
         }
     }
 }
+
+ */
+
+
+
+
+/*
+ViewModelProviders中的default factory是这样定义的:
+
+public static class DefaultFactory extends ViewModelProvider.NewInstanceFactory {
+    private Application mApplication;
+
+    public DefaultFactory(@NonNull Application application) {
+        mApplication = application;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (AndroidViewModel.class.isAssignableFrom(modelClass)) {
+            //noinspection TryWithIdenticalCatches
+            try {
+                return modelClass.getConstructor(Application.class).newInstance(mApplication);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
+            } catch (InstantiationException e) {
+                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
+            }
+        }
+        return super.create(modelClass);
+    }
+}
+
+我们的自定义Factory, 也是参考至此的.
 
  */
