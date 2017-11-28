2cf8a3cd79af7d84ee8914214a0a8b2734fed801
songzhw <songzhw2012@gmail.com>
2017-11-27 22:49:17 -0500

    Architecture Component Library Demo : ViewModelFactory

---
+++ ArchComponentDemo/app/src/main/java/ca/six/archdemo/intro/vmfactotry/DemoVm.java
@@ -0,0 +1,10 @@
+package ca.six.archdemo.intro.vmfactotry;
+
+import android.arch.lifecycle.ViewModel;
+
+
+public class DemoVm extends ViewModel {
+    public DemoVm(int i) {
+        System.out.println("szw DemoVm "+i);
+    }
+}
--- ArchComponentDemo/app/src/main/java/ca/six/archdemo/intro/vmfactotry/VmFactoryDemo.java
+++ ArchComponentDemo/app/src/main/java/ca/six/archdemo/intro/vmfactotry/VmFactoryDemo.java
@@ -1,10 +1,22 @@
 package ca.six.archdemo.intro.vmfactotry;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import ca.six.archdemo.intro.trap1.DupliViewModel;
 
 
 public class VmFactoryDemo extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        DemoVmFactory factory = new DemoVmFactory();
+        DemoVm vm = ViewModelProviders.of(this, factory).get(DemoVm.class);
 
+//        DupliViewModel vm2 = ViewModelProviders.of(this, factory).get(DupliViewModel.class);
+    }
 }
