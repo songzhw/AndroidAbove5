22e314f0db28006e9a3f3dcb10df0c04bc6354c7
songzhw <songzhw2012@gmail.com>
2017-11-30 20:30:17 -0500

    autofill

--- Android8x/app/src/main/java/six/ca/and8/autofill/AutoFillDemo.kt
+++ Android8x/app/src/main/java/six/ca/and8/autofill/AutoFillDemo.kt
@@ -1,11 +1,20 @@
 package six.ca.and8.autofill
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import six.ca.and8.R
 
 class AutoFillDemo : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login);
+
+        btnLogin.setOnClickListener({
+            startActivity(Intent(this@AutoFillDemo, AutoFillDemo::class.java))
+        })
+
     }
 }
