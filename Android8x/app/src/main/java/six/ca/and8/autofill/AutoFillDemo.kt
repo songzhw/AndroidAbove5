00a07ffd43f6e0b4050afe7aee97242e03da1450
songzhw <songzhw2012@gmail.com>
2017-11-30 20:30:38 -0500

    autofill

--- Android8x/app/src/main/java/six/ca/and8/autofill/AutoFillDemo.kt
+++ Android8x/app/src/main/java/six/ca/and8/autofill/AutoFillDemo.kt
@@ -3,6 +3,7 @@ package six.ca.and8.autofill
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.autofill.AutofillManager
 import kotlinx.android.synthetic.main.activity_login.*
 import six.ca.and8.R
 
@@ -11,6 +12,14 @@ class AutoFillDemo : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login);
+
+        val afm = getSystemService(AutofillManager::class.java)
+        if(afm.isEnabled){
+            println("szw autofill enabled : ${afm.isAutofillSupported} : ${afm.hasEnabledAutofillServices()} ")
+        } else {
+            println("szw autofill disabled : ${afm.isAutofillSupported} : ${afm.hasEnabledAutofillServices()}")
+        }
+
 
         btnLogin.setOnClickListener({
             startActivity(Intent(this@AutoFillDemo, AutoFillDemo::class.java))
