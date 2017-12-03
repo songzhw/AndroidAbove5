a8c5ca157f3c6c4a72ed9a00967ab8e83f8422e9
songzhw <songzhw2012@gmail.com>
2017-12-03 09:12:16 -0500

    ctlay

---
+++ AppCompatDemo/app/src/main/java/ca/six/demo/ctlay/demo/RelativeFillDemo.java
@@ -0,0 +1,16 @@
+package ca.six.demo.ctlay.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import ca.six.demo.R;
+
+
+public class RelativeFillDemo extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ctlay_relative);
+    }
+}
