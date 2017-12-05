16d06516410dfb82f52c8617bbe74f029f926bd6
songzhw <songzhw2012@gmail.com>
2017-12-04 19:52:38 -0500

    ctlay percentage(since 1.1.0)

---
+++ AppCompatDemo/app/src/main/java/ca/six/demo/ctlay/demo/CtlaySizeDemo.java
@@ -0,0 +1,16 @@
+package ca.six.demo.ctlay.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import ca.six.demo.R;
+
+
+public class CtlaySizeDemo extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ctlay_size);
+    }
+}
