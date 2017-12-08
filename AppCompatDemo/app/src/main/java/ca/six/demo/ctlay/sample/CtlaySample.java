c6f26d6c04015139e8baf8a9530444cb62f94d35
songzhw <songzhw2012@gmail.com>
2017-12-08 01:20:03 -0500

    ctlay two view whose weight is 0 and 1

--- AppCompatDemo/app/src/main/java/ca/six/demo/ctlay/sample/CtlaySample.java
+++ AppCompatDemo/app/src/main/java/ca/six/demo/ctlay/sample/CtlaySample.java
@@ -13,7 +13,7 @@ public class CtlaySample extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ctlay_sample_simple_weight_two);
+        setContentView(R.layout.ctlay_sample_weight_zero_one);
 
 
     }
@@ -22,8 +22,8 @@ public class CtlaySample extends Activity {
 /*
 1. ctlay_sample_simple_weight : 模仿llay的weight. 一行三个Button, weight全为1
     (app:layout_constraintWidth_default="percent" app:layout_constraintWidth_percent="0.33". 但空白也得是View, 而且还有percentage, 是0.005的百分比)
-2. ctlay_sample_simple_weight_two : 用chain. 那这就看需求, 得有不同chain style. 比如说我们最左最右没空隙, 那就是用SpreadInside.
-    ()
-3.
+2. ctlay_sample_simple_weight_two : 用chain. 那这就看需求, 得有不同chain style. 根据要宽度长达33%的特性, 得变成weight chain
+
+3. ctlay_sample_weight_zero_one : 模仿llay中2个view, 一个weight为0, 一个weight为1.
     ()
  */
