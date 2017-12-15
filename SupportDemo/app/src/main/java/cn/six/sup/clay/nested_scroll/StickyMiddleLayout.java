56e58e9da3ffac5f0a517c318a6a30405d98e7a1
songzhw <songzhw2012@gmail.com>
2017-12-15 12:40:25 -0500

    nested scroll mechanism

--- SupportDemo/app/src/main/java/cn/six/sup/clay/nested_scroll/StickyMiddleLayout.java
+++ SupportDemo/app/src/main/java/cn/six/sup/clay/nested_scroll/StickyMiddleLayout.java
@@ -1,13 +1,19 @@
 package cn.six.sup.clay.nested_scroll;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v4.view.NestedScrollingParent;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.OverScroller;
+
+import java.lang.reflect.Method;
 
 
 public class StickyMiddleLayout extends LinearLayout implements NestedScrollingParent {
@@ -31,22 +37,60 @@ public class StickyMiddleLayout extends LinearLayout implements NestedScrollingP
         scroller = new OverScroller(ctx);
     }
 
+    private int getHasVirtualKey() {
+        int dpi = 0;
+        Activity actv = (Activity) this.getContext();
+        Display display = actv.getWindowManager().getDefaultDisplay();
+        DisplayMetrics dm = new DisplayMetrics();
+        @SuppressWarnings("rawtypes")
+        Class c;
+        try {
+            c = Class.forName("android.view.Display");
+            @SuppressWarnings("unchecked")
+            Method method = c.getMethod("getRealMetrics", DisplayMetrics.class);
+            method.invoke(display, dm);
+            dpi = dm.heightPixels;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dpi;
+    }
+
+    private int getStatusHeight() {
+        int statusBarHeight1 = -1;
+        //获取status_bar_height资源的ID
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            //根据资源ID获取响应的尺寸值
+            statusBarHeight1 = getResources().getDimensionPixelSize(resourceId);
+        }
+        return statusBarHeight1;
+    }
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec); //不加这一句, 后面的childView.getMeasureHeight()的值就是0 !
 
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        int widthPixels = displayMetrics.widthPixels;
+        int heightPixels = displayMetrics.heightPixels;
+        System.out.println("szw phone1 : " + heightPixels);
+        System.out.println("szw phone2 : " + getHasVirtualKey());
+        System.out.println("szw phone3 : " + getStatusHeight());
+
+        System.out.println("szw measure : " + getMeasuredHeight());
+        ViewGroup.LayoutParams lp = getChildAt(2).getLayoutParams();
+        lp.height = getMeasuredHeight() - getChildAt(1).getMeasuredHeight();
+
         int mode = MeasureSpec.getMode(heightMeasureSpec);
         int height = MeasureSpec.getSize(heightMeasureSpec);
 
-        getChildAt(2).measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-
-        if(mode == MeasureSpec.AT_MOST){
-            System.out.println("szw measure at_most : "+getChildCount());
+        if (mode == MeasureSpec.AT_MOST) {
             height = 0;
             int childCount = getChildCount();
-            for(int i = 0; i < childCount; i++){
+            for (int i = 0; i < childCount; i++) {
                 int childHeight = getChildAt(i).getMeasuredHeight();
-                System.out.println("szw measure child height = "+childHeight);
+                System.out.println("szw measure child height = " + childHeight);
                 height += childHeight;
             }
         }
