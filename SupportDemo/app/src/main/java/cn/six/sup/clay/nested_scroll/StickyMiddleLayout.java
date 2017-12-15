66d1a85f0f6c1ab70c4a99e1f52649dd6c542b1a
songzhw <songzhw2012@gmail.com>
2017-12-14 21:41:34 -0500

    nested scroll mechanism

--- SupportDemo/app/src/main/java/cn/six/sup/clay/nested_scroll/StickyMiddleLayout.java
+++ SupportDemo/app/src/main/java/cn/six/sup/clay/nested_scroll/StickyMiddleLayout.java
@@ -2,24 +2,33 @@ package cn.six.sup.clay.nested_scroll;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.v4.view.NestedScrollingParent;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.support.v4.view.NestedScrollingParent;
-
-
+import android.widget.OverScroller;
 
 
 public class StickyMiddleLayout extends LinearLayout implements NestedScrollingParent {
+    // OverScroller class encapsulates scrolling with the ability to overshoot the bounds of a scrolling operation.
+    // This class is a drop-in replacement for android.widget.Scroller in most cases.
+    private OverScroller scroller;
+
     private int topViewHeight;
 
     public StickyMiddleLayout(Context context) {
         super(context);
+        init(context);
     }
 
     public StickyMiddleLayout(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(context);
+    }
+
+    private void init(Context ctx) {
+        scroller = new OverScroller(ctx);
     }
 
     @Override
@@ -35,36 +44,58 @@ public class StickyMiddleLayout extends LinearLayout implements NestedScrollingP
         return (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
     }
 
-    /**@param dy : 往上推, 让topView不见时, dy是正数; 反之, 是负数 */
+    /**
+     * @param dy : 往上推, 让topView不见时, dy是正数; 反之, 是负数
+     */
     @Override
     public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
         // 滑动起来了, 我作为父layout, 要不要消耗掉这些滑动, 来做我自己相关的动画
         int scrollY = getScrollY();
-        System.out.println("szw scrollY = "+scrollY);
+        System.out.println("szw scrollY = " + scrollY);
         boolean isTopNotTotallyHidden = dy > 0 && scrollY < topViewHeight;
         boolean isTopNotTotallyShown = dy < 0 && scrollY > 0
                 && !ViewCompat.canScrollVertically(target, -1); //negative num is scrolling up
-        if(isTopNotTotallyHidden || isTopNotTotallyShown) {
+        if (isTopNotTotallyHidden || isTopNotTotallyShown) {
             scrollBy(0, dy);
             consumed[1] = dy;
         }
     }
 
-    /** @return true if this parent consumed or otherwise reacted to the fling */
+
+    /**
+     * @return true if this parent consumed the fling ahead of the target view
+     */
     @Override
-    public boolean onNestedFling(View target, float velocityX, float velocityY, boolean consumed) {
+    public boolean onNestedPreFling(View target, float velocityX, float velocityY) {
         // topView已经不见, 那parent就不管了, 要滑动与fling也是给带滑动的子view了, 所以这时要返回false
         if (getScrollY() > topViewHeight) {
             return false;
         }
 
-        fling( (int) velocityY);
+        fling((int) velocityY);
         return true;  // 反之, 那我们parent就要接掌所有的滑动, 所以这里要返回true
     }
 
     private void fling(int velocityY) {
-
+        scroller.fling(0, getScrollY(), 0, velocityY, 0, 0, 0, topViewHeight);
+        invalidate();
     }
+
+    @Override
+    public void computeScroll() {
+        if (scroller.computeScrollOffset()) {
+            scrollTo(0, scroller.getCurrY());
+            invalidate();
+        }
+    }
+
+    @Override
+    public void scrollTo(int x, int y) {
+        if (y < 0) y = 0;
+        if (y > topViewHeight) y = topViewHeight;
+        if (y != getScrollY()) super.scrollTo(x, y);
+    }
+
 
 }
 
