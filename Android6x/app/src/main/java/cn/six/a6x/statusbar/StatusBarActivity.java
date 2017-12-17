d9779cdf01882ac43a7fe73341c4acf280f1d224
songzhw <songzhw2012@gmail.com>
2017-12-16 21:00:45 -0500

    status bar(and6+)

--- Android6x/app/src/main/java/cn/six/a6x/statusbar/StatusBarActivity.java
+++ Android6x/app/src/main/java/cn/six/a6x/statusbar/StatusBarActivity.java
@@ -34,7 +34,7 @@ public class StatusBarActivity extends AppCompatActivity {
         Window window = getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE );
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(Color.TRANSPARENT);
 
