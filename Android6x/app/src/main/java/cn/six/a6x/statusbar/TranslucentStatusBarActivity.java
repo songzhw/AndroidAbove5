a955a28aa1f9f15750e2c623785fdb6f75e13454
songzhw <songzhw2012@gmail.com>
2017-12-17 11:19:09 -0500

    status bar

---
+++ Android6x/app/src/main/java/cn/six/a6x/statusbar/TranslucentStatusBarActivity.java
@@ -0,0 +1,30 @@
+package cn.six.a6x.statusbar;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import cn.six.a6x.R;
+
+
+public class TranslucentStatusBarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_bar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
