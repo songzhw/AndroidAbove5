0b39a18df22ab8c479a1d2be7cf75aeea4d23d1a
songzhw <songzhw2012@gmail.com>
2017-12-20 19:41:38 -0500

    api implementation compile

--- NewDependencyDemo/app/src/main/java/ca/six/dep/MainActivity.java
+++ NewDependencyDemo/app/src/main/java/ca/six/dep/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import ca.six.biz.BizVersion;
-import ca.six.common.CommonVersion;
+//import ca.six.common.CommonVersion;
 
 public class MainActivity extends AppCompatActivity {
 
