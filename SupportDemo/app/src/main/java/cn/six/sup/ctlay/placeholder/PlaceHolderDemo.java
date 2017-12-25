45b383fc8d8857ee2099dfe721b4f426403c20e7
songzhw <songzhw2012@gmail.com>
2017-12-25 16:50:52 -0500

    dynamic ctlay placeHolder (no anim)

--- SupportDemo/app/src/main/java/cn/six/sup/ctlay/placeholder/PlaceHolderDemo.java
+++ SupportDemo/app/src/main/java/cn/six/sup/ctlay/placeholder/PlaceHolderDemo.java
@@ -3,15 +3,32 @@ package cn.six.sup.ctlay.placeholder;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.Placeholder;
+import android.view.View;
 
 import cn.six.sup.R;
 
 
-public class PlaceHolderDemo extends Activity {
+public class PlaceHolderDemo extends Activity implements View.OnClickListener {
+    private Placeholder placeholderButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_holder_demo);
+
+        placeholderButton = findViewById(R.id.template_action_button);
+
+        findViewById(R.id.ib_save).setOnClickListener(this);
+        findViewById(R.id.ib_edit).setOnClickListener(this);
+        findViewById(R.id.ib_delete).setOnClickListener(this);
+        findViewById(R.id.ib_cancel).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        placeholderButton.setContentId(id);
     }
 }
