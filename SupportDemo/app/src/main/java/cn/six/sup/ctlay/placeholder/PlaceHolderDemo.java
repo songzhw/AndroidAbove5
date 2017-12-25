9f1e33aa4ba13041bfa57e6d77266888f89e999a
songzhw <songzhw2012@gmail.com>
2017-12-24 19:22:38 -0500

    ctlay placeHolder

---
+++ SupportDemo/app/src/main/java/cn/six/sup/ctlay/placeholder/PlaceHolderDemo.java
@@ -0,0 +1,17 @@
+package cn.six.sup.ctlay.placeholder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import cn.six.sup.R;
+
+
+public class PlaceHolderDemo extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_holder_demo);
+    }
+}
