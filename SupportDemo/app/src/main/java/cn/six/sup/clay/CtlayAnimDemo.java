ce31ef5f4345453a2976715ea1ee00f8bcef4db0
songzhw <songzhw2012@gmail.com>
2017-12-27 16:45:32 -0500

    ctlay anim

--- SupportDemo/app/src/main/java/cn/six/sup/clay/CtlayAnimDemo.java
+++ SupportDemo/app/src/main/java/cn/six/sup/clay/CtlayAnimDemo.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.view.View;
 
 import cn.six.sup.R;
 
 
-public class CtlayAnimDemo extends Activity {
+public class CtlayAnimDemo extends Activity implements View.OnClickListener {
     private ConstraintLayout ctlay;
+    private ConstraintSet applySet = new ConstraintSet();
+    private ConstraintSet resetSet = new ConstraintSet();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,6 +21,20 @@ public class CtlayAnimDemo extends Activity {
         setContentView(R.layout.activity_ctlay_anim);
 
         ctlay = findViewById(R.id.ctlayAnim);
+        applySet.clone(ctlay);
+        resetSet.clone(ctlay);
 
+        findViewById(R.id.btnAnimApply).setOnClickListener(this);
+        findViewById(R.id.btnAnimReset).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btnAnimApply) {
+
+        } else if (id == R.id.btnAnimReset) {
+
+        }
     }
 }
