cf2f999dd5c93559b47ba8044df2bb141bc128ca
songzhw <songzhw2012@gmail.com>
2018-01-25 16:16:43 -0500

    bug: at the bottom, scroll down. Rv does not reach the top, but the top image shows somehow) (not fixed)

--- SupportDemo/app/src/main/java/cn/six/sup/nested_scroll/StickyMiddleLayout.java
+++ SupportDemo/app/src/main/java/cn/six/sup/nested_scroll/StickyMiddleLayout.java
@@ -64,27 +64,35 @@ public class StickyMiddleLayout extends LinearLayout implements NestedScrollingP
 
     /**
      * @param target: 经测试, 是那个可以自己滑动的RecyclerView
-     * @param dy : 往上推, 让topView不见时, dy是正数; 反之, 是负数
+     * @param dy      : 往上推, 让topView不见时, dy是正数; 反之, 是负数
      */
     @Override
     public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
         // 滑动起来了, 我作为父layout, 要不要消耗掉这些滑动, 来做我自己相关的动画
         int scrollY = getScrollY(); //scrollY > 0 就是scroll up, 即让topView不可见, 让显示更多底部
 
-        int scrollUp = -1; // 手指的动作是向上拉, 即拉出底部来, 或说是让topView不可见. || 负数就是scroll up
-        int scrollDown = 1;//正数就是scroll down
+        int scrollUp = -1;  // 手指的动作是向上拉, 即拉出底部来, 或说是让topView不可见. || 负数就是scroll up
+        int scrollDown = 1; // 正数就是scroll down
 
         boolean isTopShowing = scrollY >= 0 && scrollY < topViewHeight;
         boolean isRvStillHasContentNotShow = target.canScrollVertically(scrollUp); //即表示底部仍有内容, 仍可以拉
         boolean isRvAtTop = !target.canScrollVertically(scrollDown);
+        System.out.println("szw scrolly = " + scrollY +
+                " ; canScrollUp? = " + target.canScrollVertically(scrollUp) +
+                " ; canScrollDown? = " + target.canScrollVertically(scrollDown));
 
-        if ( (isTopShowing && isRvStillHasContentNotShow && dy > 0)
-                || isRvAtTop && dy < 0) {
+        if ((isTopShowing && isRvStillHasContentNotShow && dy > 0)
+                || (isTopShowing && isRvAtTop && dy < 0)) {
             scrollBy(0, dy);
             consumed[1] = dy;
         }
     }
 
+    /*
+    以Nexus 6P手机为例,
+    最顶了, 还scrollDown,  canScrollUp? = false ; canScrollDown? = true
+    rv到底后, 还scrollUp,  canScrollUp? = true ; canScrollDown? = false
+     */
 
     // ============================= fling =============================
 
