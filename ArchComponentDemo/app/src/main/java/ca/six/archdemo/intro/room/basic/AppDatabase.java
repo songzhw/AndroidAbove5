3ac4e2fdcc01dd15a77e5101e256025824961970
songzhw <songzhw2012@gmail.com>
2018-02-01 22:07:18 -0500

    room

---
+++ ArchComponentDemo/app/src/main/java/ca/six/archdemo/intro/room/basic/AppDatabase.java
@@ -0,0 +1,11 @@
+package ca.six.archdemo.intro.room.basic;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+
+@Database(entities = {User.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract UserDao userDao();
+}
+
+
