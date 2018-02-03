c83af0ff040f5b0fb665cb284717fd9f70267d3e
songzhw <songzhw2012@gmail.com>
2018-02-03 11:00:52 -0500

    Transformations

--- ArchComponentDemo/app/src/main/java/ca/six/archdemo/intro/transfer/auto2/TransformDemo3.java
+++ ArchComponentDemo/app/src/main/java/ca/six/archdemo/intro/transfer/auto2/TransformDemo3.java
@@ -11,22 +11,27 @@ import ca.six.archdemo.R;
 
 
 public class TransformDemo3 extends AppCompatActivity {
-    TransformViewModel3 vm;
+    private TransformViewModel3 vm;
+    private Button btn1, btn2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tv_btn);
 
-        Button btn1 = findViewById(R.id.btn_simple);
-        Button btn2 = findViewById(R.id.btn_simple2);
+        btn1 = findViewById(R.id.btn_simple);
+        btn2 = findViewById(R.id.btn_simple2);
 
         vm = ViewModelProviders.of(this).get(TransformViewModel3.class);
         vm.getName().observe(this, name -> {
-            btn1.setText("name->test || " + name);
+            String text = "name->test || " + name;
+            System.out.println("szw activity get name = " + text);
+            btn1.setText(text);
         });
         vm.getPenName().observe(this, penName -> {
-            btn2.setText("name->szw || " + penName);
+            String text = "name->szw || " + penName;
+            System.out.println("szw activity get penName = " + text);
+            btn2.setText(text);
         });
     }
 
