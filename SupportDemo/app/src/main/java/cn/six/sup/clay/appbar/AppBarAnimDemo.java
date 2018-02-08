dccd01e9c1f1bb3d67b3a3656b3f39172445d5a8
songzhw <songzhw2012@gmail.com>
2018-02-07 20:48:55 -0500

    appbar onOffsetChanged

---
+++ SupportDemo/app/src/main/java/cn/six/sup/clay/appbar/AppBarAnimDemo.java
@@ -0,0 +1,18 @@
+package cn.six.sup.clay.appbar;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.six.sup.R;
+
+
+public class AppBarAnimDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appbar_anim);
+    }
+
+}
