5be7dd31895b30fd9eac1f437e55151d178908ad
songzhw <songzhw2012@gmail.com>
2018-02-21 19:06:19 -0500

    refactor

--- SupportDemo/app/src/main/java/cn/six/sup/rv/dragdrop/groups_albe/DragRecyclerViewDemo3.java
+++ SupportDemo/app/src/main/java/cn/six/sup/rv/dragdrop/groups_albe/DragRecyclerViewDemo3.java
@@ -136,14 +136,14 @@ public class DragRecyclerViewDemo3 extends Activity implements RvItemDragSwipeLi
 
         Collections.sort(bottoms, (c1, c2) -> c1.name.compareTo(c2.name));
 
+        // add the two line below to make sure the drag&drop would be weird after the reordering
         LinearLayoutManager layMgr = (LinearLayoutManager)rv.getLayoutManager();
         int firstVisblePosition = layMgr.findFirstVisibleItemPosition();
 
-        // add the two line below to make sure the drag&drop would be weird after the reordering
-        adapter = new DragDropRvAdapter3(this.data);
-        rv.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
 
         rv.scrollToPosition(firstVisblePosition);
+
     }
 
 }
