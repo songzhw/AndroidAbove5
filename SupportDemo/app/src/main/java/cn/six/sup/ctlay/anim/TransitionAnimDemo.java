69c89747ef4682bdd10b864aa4f8626c4c559e06
songzhw <songzhw2012@gmail.com>
2018-02-22 20:50:12 -0500

    ctlay animation

---
+++ SupportDemo/app/src/main/java/cn/six/sup/ctlay/anim/TransitionAnimDemo.java
@@ -0,0 +1,20 @@
+package cn.six.sup.ctlay.anim;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import cn.six.sup.R;
+
+
+public class TransitionAnimDemo extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actv_food_detail);
+    }
+
+
+
+}
