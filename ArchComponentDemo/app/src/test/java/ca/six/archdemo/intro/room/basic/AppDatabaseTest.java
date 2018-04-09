434d8c452ea5a146a70be1092145883e19bc5793
songzhw <songzhw2012@gmail.com>
2018-04-08 22:58:05 -0400

    test ROOM

--- ArchComponentDemo/app/src/test/java/ca/six/archdemo/intro/room/basic/AppDatabaseTest.java
+++ ArchComponentDemo/app/src/test/java/ca/six/archdemo/intro/room/basic/AppDatabaseTest.java
@@ -1,5 +1,6 @@
 package ca.six.archdemo.intro.room.basic;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
 
 import android.arch.persistence.room.Room;
@@ -10,18 +11,35 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 
+import java.util.Date;
+import java.util.List;
+
 @RunWith(RobolectricTestRunner.class)
 public class AppDatabaseTest {
     private AppDatabase db;
+    private UserDao dao;
 
     @Before
     public void setUp() {
-        db = Room.databaseBuilder(RuntimeEnvironment.application, AppDatabase.class, "db_name").build();
+        db = Room.inMemoryDatabaseBuilder(RuntimeEnvironment.application, AppDatabase.class).build();
+        dao = db.userDao();
     }
 
     @Test
     public void f1() {
-        User user  = db.userDao().findByName("mike", "jorden");
+        System.out.println("szw1 Thread = "+Thread.currentThread().getName());
+        User user  = dao.findByName("jorden");
         assertNull(user);
+    }
+
+    @Test
+    public void f2(){
+        System.out.println("szw2 Thread = "+Thread.currentThread().getName());
+        dao.insertAll(new User(100, "jim", new Address("ny",100), new Date(1900, 1, 1)));
+        dao.insertAll(new User(200, "lee", new Address("ny",200), new Date(1910, 1, 1)));
+        dao.insertAll(new User(300, "kim", new Address("ny",300), new Date(1980, 1, 1)));
+
+        List<User> oldUsers = dao.findUserBornIn(new Date(1900, 1,1), new Date(1920, 12, 31));
+        assertEquals(2, oldUsers.size());
     }
 }
