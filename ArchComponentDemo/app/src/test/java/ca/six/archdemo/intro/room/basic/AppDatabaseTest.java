fa533e6d9a5dccf13021d9dce8255007388434d1
songzhw <songzhw2012@gmail.com>
2018-04-08 23:03:39 -0400

    test ROOM: error, 因为运行在thread中, 所以不管怎么样都会显示成功

--- ArchComponentDemo/app/src/test/java/ca/six/archdemo/intro/room/basic/AppDatabaseTest.java
+++ ArchComponentDemo/app/src/test/java/ca/six/archdemo/intro/room/basic/AppDatabaseTest.java
@@ -10,11 +10,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
 
 import java.util.Date;
 import java.util.List;
 
+import ca.six.archdemo.BuildConfig;
+
 @RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
 public class AppDatabaseTest {
     private AppDatabase db;
     private UserDao dao;
@@ -27,19 +31,22 @@ public class AppDatabaseTest {
 
     @Test
     public void f1() {
-        System.out.println("szw1 Thread = "+Thread.currentThread().getName());
-        User user  = dao.findByName("jorden");
-        assertNull(user);
+        new Thread( () -> {
+            User user  = dao.findByName("jorden");
+            assertNull(user);
+        }).start();
+
     }
 
     @Test
     public void f2(){
-        System.out.println("szw2 Thread = "+Thread.currentThread().getName());
-        dao.insertAll(new User(100, "jim", new Address("ny",100), new Date(1900, 1, 1)));
-        dao.insertAll(new User(200, "lee", new Address("ny",200), new Date(1910, 1, 1)));
-        dao.insertAll(new User(300, "kim", new Address("ny",300), new Date(1980, 1, 1)));
+        new Thread( () -> {
+            dao.insertAll(new User(100, "jim", new Address("ny",100), new Date(1900, 1, 1)));
+            dao.insertAll(new User(200, "lee", new Address("ny",200), new Date(1910, 1, 1)));
+            dao.insertAll(new User(300, "kim", new Address("ny",300), new Date(1980, 1, 1)));
 
-        List<User> oldUsers = dao.findUserBornIn(new Date(1900, 1,1), new Date(1920, 12, 31));
-        assertEquals(2, oldUsers.size());
+            List<User> oldUsers = dao.findUserBornIn(new Date(1900, 1,1), new Date(1920, 12, 31));
+            assertEquals(3, oldUsers.size());
+        }).start();
     }
 }
