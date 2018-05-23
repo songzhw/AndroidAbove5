d54c83aa9e3f5bb0cb308a3a121bf2c33d17468a
songzhw <songzhw2012@gmail.com>
2018-05-22 23:04:23 -0400

    navigation error: (on clicking) IllegalStateException: no current navigation node

--- IO2018/app/src/main/java/ca/six/io2018/biz/main/MainFragment.kt
+++ IO2018/app/src/main/java/ca/six/io2018/biz/main/MainFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import ca.six.io2018.R
 
@@ -18,13 +20,19 @@ class MainFragment : NavHostFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val viewWhole = inflater.inflate(R.layout.fragment_main, container, false)
+        val tvMainMessage = viewWhole.findViewById<TextView>(R.id.tvMainMessage)
+        tvMainMessage.setOnClickListener { tv ->
+            // findNavController(view): @param view -- the view to search from
+            Navigation.findNavController(viewWhole)
+                    .navigate(R.id.nav_action_main_to_settings)
+        }
+        return viewWhole
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
