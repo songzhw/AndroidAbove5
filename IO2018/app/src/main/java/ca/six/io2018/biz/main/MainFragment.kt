fbc3cd168cf14e70863de87cf5361fdb1d961e7f
songzhw <songzhw2012@gmail.com>
2018-05-22 22:46:26 -0400

    navigation

--- IO2018/app/src/main/java/ca/six/io2018/biz/main/MainFragment.kt
+++ IO2018/app/src/main/java/ca/six/io2018/biz/main/MainFragment.kt
@@ -2,7 +2,6 @@ package ca.six.io2018.biz.main
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,7 +18,7 @@ class MainFragment : NavHostFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
---
+++ IO2018/app/src/main/java/ca/six/io2018/biz/settings/SettingsActivity.kt
@@ -0,0 +1,17 @@
+package ca.six.io2018.biz.settings
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import ca.six.io2018.R
+
+import kotlinx.android.synthetic.main.activity_settings.*
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+
+}
