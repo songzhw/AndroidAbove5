a8322771243f7648750c54593d635f5e6ac85981
songzhw <songzhw2012@gmail.com>
2018-06-28 15:34:04 -0400

    ctlay: parallex rv (ing..)

--- ConstraintLayoutDemo/app/src/main/java/ca/six/ctlay/anim/parallex/ParallexRvDemo.kt
+++ ConstraintLayoutDemo/app/src/main/java/ca/six/ctlay/anim/parallex/ParallexRvDemo.kt
@@ -3,6 +3,7 @@ package ca.six.ctlay.anim.parallex
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.PagerSnapHelper
 import ca.six.advk.utils.rv.OneAdapter
 import ca.six.advk.utils.rv.RvViewHolder
 import ca.six.ctlay.R
@@ -23,8 +24,12 @@ class ParallexRvDemo : AppCompatActivity() {
             }
         }
 
+
         rvParallex.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvParallex.adapter = adapter
+
+        PagerSnapHelper().attachToRecyclerView(rvParallex)
+
 
     }
 
