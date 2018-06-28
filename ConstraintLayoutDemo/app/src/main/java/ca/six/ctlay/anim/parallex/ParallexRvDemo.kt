0baf1876806e1786adfda13049955f210b072785
songzhw <songzhw2012@gmail.com>
2018-06-28 15:32:13 -0400

    ctlay: parallex rv (ing..)

--- ConstraintLayoutDemo/app/src/main/java/ca/six/ctlay/anim/parallex/ParallexRvDemo.kt
+++ ConstraintLayoutDemo/app/src/main/java/ca/six/ctlay/anim/parallex/ParallexRvDemo.kt
@@ -8,19 +8,20 @@ import ca.six.advk.utils.rv.RvViewHolder
 import ca.six.ctlay.R
 import kotlinx.android.synthetic.main.activity_rv_parallex.*
 
-class ParallexRvDemo : AppCompatActivity(){
+class ParallexRvDemo : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rv_parallex)
 
-        val data  = IntArray(5, {it}).asList()
+        val data = intArrayOf(R.drawable.one_bg, R.drawable.two_bg, R.drawable.three_bg,
+                R.drawable.fourth_bg, R.drawable.fifth_bg).asList()
         val adapter = object : OneAdapter<Int>(R.layout.item_rv_parralex, data) {
-            override fun apply(vh: RvViewHolder, t: Int, position: Int) {
-
+            override fun apply(vh: RvViewHolder, drawableId: Int, position: Int) {
+                vh.setSrc(R.id.ivItemRvParallex, drawableId)
+                vh.setText(R.id.tvItemRvParallex, "Item ${position + 1}")
             }
         }
-
 
         rvParallex.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvParallex.adapter = adapter
