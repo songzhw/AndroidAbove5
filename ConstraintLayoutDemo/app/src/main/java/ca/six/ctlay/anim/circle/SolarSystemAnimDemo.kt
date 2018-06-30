898342b62a9dec05626e5c905771163117fabc2e
songzhw <songzhw2012@gmail.com>
2018-06-30 19:22:39 -0400

    ctlay solar system animation

---
+++ ConstraintLayoutDemo/app/src/main/java/ca/six/ctlay/anim/circle/SolarSystemAnimDemo.kt
@@ -0,0 +1,10 @@
+package ca.six.ctlay.anim.circle
+
+import android.support.v7.app.AppCompatActivity
+
+class SolarSystemAnimDemo : AppCompatActivity(){
+}
+
+// ConstraintLayoutParams.circleConstraint
+// ConstraintLayoutParams.circleAngle
+// ConstraintLayoutParams.circleRadius
