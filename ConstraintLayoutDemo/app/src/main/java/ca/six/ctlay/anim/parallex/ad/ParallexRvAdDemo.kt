28ecbfc8f1cd1310f3295a03683932e16eb7b0ff
songzhw <songzhw2012@gmail.com>
2018-07-10 22:36:53 -0400

    ctlay animation: parallex2

--- ConstraintLayoutDemo/app/src/main/java/ca/six/ctlay/anim/parallex/ad/ParallexRvAdDemo.kt
+++ ConstraintLayoutDemo/app/src/main/java/ca/six/ctlay/anim/parallex/ad/ParallexRvAdDemo.kt
@@ -2,6 +2,7 @@ package ca.six.ctlay.anim.parallex.ad
 
 import android.graphics.Rect
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
@@ -11,6 +12,8 @@ import ca.six.ctlay.R
 import ca.six.ctlay.utils.rv.compose.ItemView
 import ca.six.ctlay.utils.rv.compose.OneTypesAdapter
 import kotlinx.android.synthetic.main.activity_rv_parallex.*
+
+// TODO : 这个是要求广告位图片是有一定尺寸要求的. 好在这个好弄, 后台上传符合尺寸的图片就好
 
 class ParallexRvAdDemo : AppCompatActivity() {
     private lateinit var layoutManager: LinearLayoutManager
@@ -48,20 +51,20 @@ class ParallexRvAdDemo : AppCompatActivity() {
                     val itemView = layoutManager.findViewByPosition(i)  // 高度桓为630, 广告位的高度也是630 (但其drawable高为2100)
                     val adView = itemView.findViewById<ImageView>(R.id.ivItemParallexAd)
                     if (adView != null) { // 说明这一行是广告位
+
                         val itemHeight = itemView.height
                         val adDrawableRect = Rect()
                         adView.getDrawingRect(adDrawableRect)  //adDrawableRect.height()是2100
-//                        println("szw offset = $offset , top = ${itemView.top}  ,  itemHeight = $itemHeight, imageHeight = ${adDrawableRect.height()} ,  rvHeight = ${layoutManager.height}")
-
                         val maxPercent : Float = 0.5f * adDrawableRect.height() / itemHeight
-                        val minPercent : Float = 0f
 
                         val lastItemTop = rvHeight - itemHeight   // 1482
                         val itemTop = Math.max(0, itemView.top)  // 不要到负数, 在顶上还上推, 就不parallex了  [0, 1477]
                         val percent = 1.0f * itemTop / lastItemTop  // [0,1]之间
 
-                        val guidePercent = maxPercent * percent
-                        println("szw per = ${guidePercent}")
+                        val guidePercent = maxPercent * (1 - percent)
+                        val lp = itemView.layoutParams as ConstraintLayout.LayoutParams
+                        lp.guidePercent = guidePercent
+                        itemView.layoutParams = lp
                     }
                 }
             }
