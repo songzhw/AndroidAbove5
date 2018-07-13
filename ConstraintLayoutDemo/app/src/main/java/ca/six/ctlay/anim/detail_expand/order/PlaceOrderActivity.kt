9249017d952fa9cd35769bf70f8d47a74618ffad
songzhw <songzhw2012@gmail.com>
2018-07-12 23:46:49 -0400

    ctlay animation: place order

--- ConstraintLayoutDemo/app/src/main/java/ca/six/ctlay/anim/detail_expand/order/PlaceOrderActivity.kt
+++ ConstraintLayoutDemo/app/src/main/java/ca/six/ctlay/anim/detail_expand/order/PlaceOrderActivity.kt
@@ -3,7 +3,6 @@ package ca.six.ctlay.anim.detail_expand.order
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.Window
 import ca.six.ctlay.R
 import kotlinx.android.synthetic.main.activity_place_order.*
 
