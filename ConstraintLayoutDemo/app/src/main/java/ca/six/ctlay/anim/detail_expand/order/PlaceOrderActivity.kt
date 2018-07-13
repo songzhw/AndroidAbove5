31fd70a3ccde6c6659419bab785c1da86f5eeac4
songzhw <songzhw2012@gmail.com>
2018-07-12 23:31:07 -0400

    ctlay animation: place order

--- ConstraintLayoutDemo/app/src/main/java/ca/six/ctlay/anim/detail_expand/order/PlaceOrderActivity.kt
+++ ConstraintLayoutDemo/app/src/main/java/ca/six/ctlay/anim/detail_expand/order/PlaceOrderActivity.kt
@@ -1,15 +1,21 @@
 package ca.six.ctlay.anim.detail_expand.order
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Window
 import ca.six.ctlay.R
+import kotlinx.android.synthetic.main.activity_place_order.*
 
 class PlaceOrderActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_place_order)
+
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationIcon(R.drawable.ic_nav_back)
+        toolbar.setTitle("Place Order")
+        toolbar.setTitleTextColor(Color.WHITE)
     }
 }
