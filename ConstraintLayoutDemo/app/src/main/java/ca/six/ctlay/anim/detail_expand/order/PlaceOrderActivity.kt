436be0b18f13d47d486aae38d5325a86f4056d60
songzhw <songzhw2012@gmail.com>
2018-07-13 22:29:35 -0400

    ctlay animation - place order : add animations to both layout

--- ConstraintLayoutDemo/app/src/main/java/ca/six/ctlay/anim/detail_expand/order/PlaceOrderActivity.kt
+++ ConstraintLayoutDemo/app/src/main/java/ca/six/ctlay/anim/detail_expand/order/PlaceOrderActivity.kt
@@ -2,11 +2,15 @@ package ca.six.ctlay.anim.detail_expand.order
 
 import android.graphics.Color
 import android.os.Bundle
+import android.support.constraint.ConstraintSet
 import android.support.v7.app.AppCompatActivity
+import android.transition.ChangeBounds
+import android.transition.TransitionManager
 import ca.six.ctlay.R
 import kotlinx.android.synthetic.main.activity_place_order.*
 
 class PlaceOrderActivity : AppCompatActivity() {
+    private var isExpaned = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +21,19 @@ class PlaceOrderActivity : AppCompatActivity() {
         toolbar.setTitle("Place Order")
         toolbar.setTitleTextColor(Color.WHITE)
 
-        viewCard.setOnClickListener{ v ->
-            // TODO constraintSet change, and animation with TransitionManager
+
+
+        viewCard.setOnClickListener { v ->
+            // 若已经是expanded状态, 那再点击就要转为缩回状态
+            val layoutResId = if (isExpaned) R.layout.activity_place_order else R.layout.layout_place_order_expanded
+            val constraintSet = ConstraintSet()
+            constraintSet.clone(this, layoutResId)
+
+            TransitionManager.beginDelayedTransition(ctlayPlaceOrder, ChangeBounds())
+            constraintSet.applyTo(ctlayPlaceOrder)
+
+            isExpaned = !isExpaned
         }
     }
+
 }
