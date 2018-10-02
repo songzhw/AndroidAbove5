1605a769ae2011645c604b35f73708e5831b1345
songzhw <songzhw2012@gmail.com>
2018-10-01 22:11:30 -0400

    init a project for Android P(9.0, api 28)

---
+++ Android9x/ReadMe.md
@@ -0,0 +1,15 @@
+Android P (API Level 28, Android 9.0)
+
+1. 适配 "刘海头" (display cutout support)
+
+2. 非SDK接口的限制
+
+3. ImageDecoder 代替BitmapFactory, BitmapFactory.Options
+
+4. AnimatedImageDrawable, 用来绘制gif/webp的动画图
+
+5. 屏幕旋转有大变化
+
+6. Apache的HttpClient库被最终移除了 (以前只是不推荐用, 要强用仍是行. 现在不行了)
+
+7.
---
+++ Android9x/app/src/main/java/ca/six/ax9/NoHttpDemo.kt
@@ -0,0 +1,21 @@
+package ca.six.ax9
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class NoHttpDemo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tv_btn)
+    }
+
+    fun onClickSimpleButton(v: View) {
+
+    }
+
+    fun onClickSimpleButton2(v: View) {
+
+    }
+}
