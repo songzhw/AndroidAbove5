4ed647b362a2eb06c3c161bb0fc7ec8ddee454d5
songzhw <songzhw2012@gmail.com>
2018-10-15 16:35:54 -0400

    find no change in API 28 ?!

--- Android9x/app/src/main/java/ca/six/ax9/screen_rotate/ScreenOrigDemo.kt
+++ Android9x/app/src/main/java/ca/six/ax9/screen_rotate/ScreenOrigDemo.kt
@@ -52,7 +52,7 @@ class ScreenOrigDemo : AppCompatActivity() {
 }
 
 /*
-No configChange properties on <activity> in manifest
+[27] No configChange properties on <activity> in manifest
 
 10-15 16:10:00.160 27673-27673/ca.six.ax9 I/System.out: szw Activity01 onStart()
 10-15 16:10:00.166 27673-27673/ca.six.ax9 I/System.out: szw Activity01 onResume()
@@ -65,13 +65,27 @@ No configChange properties on <activity> in manifest
  */
 
 /*
-add "android:configChanges="keyboardHidden|keyboard|orientation|screenSize"" in the manifest
+[27] add "android:configChanges="keyboardHidden|keyboard|orientation|screenSize"" in the manifest
 
-10-15 16:13:32.541 28277-28277/ca.six.ax9 I/System.out: szw Activity01 onStart()
-10-15 16:13:32.545 28277-28277/ca.six.ax9 I/System.out: szw Activity01 onResume()
-// rotate screen
-10-15 16:13:40.143 28277-28277/ca.six.ax9 I/System.out:
-szw Activity01 : {1.15 302mcc220mnc [en_US,zh_CN_#Hans] ldltr sw411dp w683dp h387dp 560dpi nrml land finger -keyb/v/h -nav/h appBounds=Rect(168, 0 - 2560, 1440) s.23}
+10-15 16:23:45.482 3769-3769/ca.six.ax9 I/System.out: szw Activity01 onStart()
+10-15 16:23:45.483 3769-3769/ca.six.ax9 I/System.out: szw Activity01 onResume()
+//rotate
+10-15 16:23:52.068 3769-3769/ca.six.ax9 I/System.out: szw Activity01 : {1.0 ?mcc?mnc [en_US,fr_FR] ldltr sw437dp w759dp h409dp 480dpi nrml long land finger -keyb/v/h -nav/h winConfig={ mBounds=Rect(0, 0 - 2279, 1312) mAppBounds=Rect(144, 0 - 2423, 1312) mWindowingMode=fullscreen mActivityType=standard} s.116}
 
+*/
+
+
+/*
+[28] without "android:configChange" properpties in manifest
+: Same
+ */
+
+/*
+[28] android:configChanges="keyboardHidden|keyboard|orientation|screenSize"
+:
+10-15 16:21:43.920 3535-3535/ca.six.ax9 I/System.out: szw Activity01 onStart()
+10-15 16:21:43.922 3535-3535/ca.six.ax9 I/System.out: szw Activity01 onResume()
+//rotate
+10-15 16:21:54.334 3535-3535/ca.six.ax9 I/System.out: szw Activity01 : {1.0 ?mcc?mnc [en_US,fr_FR] ldltr sw437dp w759dp h409dp 480dpi nrml long land finger -keyb/v/h -nav/h winConfig={ mBounds=Rect(0, 0 - 2279, 1312) mAppBounds=Rect(144, 0 - 2423, 1312) mWindowingMode=fullscreen mActivityType=standard} s.114}
 
  */
