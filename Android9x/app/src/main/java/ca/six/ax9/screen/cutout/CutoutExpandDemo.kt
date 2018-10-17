6adad7f7683a8189d51e9ce0d4e2c622f2ebe1c5
songzhw <songzhw2012@gmail.com>
2018-10-17 10:46:20 -0400

    fullscreen and display the image in the danger zone

--- Android9x/app/src/main/java/ca/six/ax9/screen/cutout/CutoutExpandDemo.kt
+++ Android9x/app/src/main/java/ca/six/ax9/screen/cutout/CutoutExpandDemo.kt
@@ -1,17 +1,23 @@
 package ca.six.ax9.screen.cutout
 
-import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import ca.six.ax9.R
 
-class CutoutExpandDemo : AppCompatActivity(){
+class CutoutExpandDemo : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_cutout_expand)
+//        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+        val lp = window.attributes
+        lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
     }
 
 }
