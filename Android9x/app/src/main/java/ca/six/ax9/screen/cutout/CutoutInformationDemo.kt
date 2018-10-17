2f2f481037111b73c338ac38ddd16af0017b99a9
songzhw <songzhw2012@gmail.com>
2018-10-17 15:11:55 -0400

    update the cutout infomation

--- Android9x/app/src/main/java/ca/six/ax9/screen/cutout/CutoutInformationDemo.kt
+++ Android9x/app/src/main/java/ca/six/ax9/screen/cutout/CutoutInformationDemo.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.DisplayCutout
 import android.view.View
 import ca.six.ax9.R
 
@@ -18,13 +19,15 @@ class CutoutInformationDemo : AppCompatActivity() {
         val decorView = window.decorView
 
         decorView.post {
-            val displayCutout = decorView.rootWindowInsets.displayCutout
-            Log.e("szw", "安全区域距离屏幕左边的距离 SafeInsetLeft:" + displayCutout.safeInsetLeft)
-            Log.e("szw", "安全区域距离屏幕右部的距离 SafeInsetRight:" + displayCutout.safeInsetRight)
-            Log.e("szw", "安全区域距离屏幕顶部的距离 SafeInsetTop:" + displayCutout.safeInsetTop)
-            Log.e("szw", "安全区域距离屏幕底部的距离 SafeInsetBottom:" + displayCutout.safeInsetBottom)
+            val displayCutout : DisplayCutout? = decorView.rootWindowInsets.displayCutout
+            println("szw 01 : ${decorView.rootWindowInsets}")
+            println("szw 02 : ${decorView.rootWindowInsets.displayCutout}")
+            Log.e("szw", "安全区域距离屏幕左边的距离 SafeInsetLeft:" + displayCutout?.safeInsetLeft)
+            Log.e("szw", "安全区域距离屏幕右部的距离 SafeInsetRight:" + displayCutout?.safeInsetRight)
+            Log.e("szw", "安全区域距离屏幕顶部的距离 SafeInsetTop:" + displayCutout?.safeInsetTop)
+            Log.e("szw", "安全区域距离屏幕底部的距离 SafeInsetBottom:" + displayCutout?.safeInsetBottom)
 
-            val rects = displayCutout.boundingRects
+            val rects = displayCutout?.boundingRects
             if (rects == null || rects.size === 0) {
                 Log.e("szw", "不是刘海屏")
             } else {
@@ -52,4 +55,25 @@ class CutoutInformationDemo : AppCompatActivity() {
 也就是说安全区域在下方, 距离上方是137px
 而cutout在危险区域中也不是全占了, 只占[586, 726]之间的部分 (垂直是占了[0, 137]的部分)
 
+ */
+
+/*
+发现一个疑似bug:
+
+转屏后, 日志是:
+szw: 安全区域距离屏幕左边的距离 SafeInsetLeft:null
+szw: 安全区域距离屏幕右部的距离 SafeInsetRight:null
+szw: 安全区域距离屏幕顶部的距离 SafeInsetTop:null
+szw: 安全区域距离屏幕底部的距离 SafeInsetBottom:null
+szw: 不是刘海屏
+ */
+
+/*
+发现第二个疑似bug:
+加上本工程的@style/fullscreen这个theme给本Activity后, 不论横还是竖屏, 结果都是:
+szw: 安全区域距离屏幕左边的距离 SafeInsetLeft:null
+szw: 安全区域距离屏幕右部的距离 SafeInsetRight:null
+szw: 安全区域距离屏幕顶部的距离 SafeInsetTop:null
+szw: 安全区域距离屏幕底部的距离 SafeInsetBottom:null
+szw: 不是刘海屏
  */
