148d4b7c8c56c0bbd467cbc9d0452f1e78129e2c
songzhw <songzhw2012@gmail.com>
2018-10-30 12:02:18 -0400

    not fixed

--- Android8x/app/src/main/java/six/ca/and8/services/EightService.kt
+++ Android8x/app/src/main/java/six/ca/and8/services/EightService.kt
@@ -1,6 +1,10 @@
 package six.ca.and8.services
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 
@@ -16,6 +20,10 @@ class EightService : Service() {
 
 	override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
 		println("szw EightService.onStartCmd()")
+
+
+		this.startForeground(100, null);
+
 		return super.onStartCommand(intent, flags, startId)
 	}
 
