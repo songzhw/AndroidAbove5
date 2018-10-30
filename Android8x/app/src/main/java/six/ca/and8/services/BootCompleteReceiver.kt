4c41bf7a24d4dfec59b1acd9be4e665051527322
songzhw <songzhw2012@gmail.com>
2018-10-30 11:05:18 -0400

    add code for >= Android8

--- Android8x/app/src/main/java/six/ca/and8/services/BootCompleteReceiver.kt
+++ Android8x/app/src/main/java/six/ca/and8/services/BootCompleteReceiver.kt
@@ -3,11 +3,17 @@ package six.ca.and8.services
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 
 class BootCompleteReceiver : BroadcastReceiver() {
 	override fun onReceive(context: Context, intent: Intent) {
 		println("szw BootCompleteReceiver.onReceive()")
+
 		val it = Intent(context, EightService::class.java)
-		context.startService(it)
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			context.startForegroundService(it)
+		} else {
+			context.startService(it)
+		}
 	}
 }
