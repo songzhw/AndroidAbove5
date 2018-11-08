cd9cce7eec9b5155b036d21300a84e005f7a37c8
songzhw <songzhw2012@gmail.com>
2018-11-08 14:05:18 -0500

    rotate, sw600dp layout

---
+++ Android9x/app/src/main/java/ca/six/ax9/screen/rotate/RotateDemo2.kt
@@ -0,0 +1,16 @@
+package ca.six.ax9.screen.rotate
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import ca.six.ax9.R
+
+class RotateDemo2 : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_rotate_two)
+
+	}
+
+
+}
