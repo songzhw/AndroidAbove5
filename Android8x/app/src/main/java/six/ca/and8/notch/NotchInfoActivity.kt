a146b7c7199d719e427aec815551226ab9709413
songzhw <songzhw2012@gmail.com>
2018-11-14 09:44:32 -0500

    reflection to set the lp.layoutInDisplayCutoutMode

--- Android8x/app/src/main/java/six/ca/and8/notch/NotchInfoActivity.kt
+++ Android8x/app/src/main/java/six/ca/and8/notch/NotchInfoActivity.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_notch_info.*
 import six.ca.and8.R
+import java.lang.reflect.Field
 
 class NotchInfoActivity : AppCompatActivity() {
 	val BUILD_VERSION_P = 28
@@ -20,7 +21,19 @@ class NotchInfoActivity : AppCompatActivity() {
 
 		if (Build.VERSION.SDK_INT >= BUILD_VERSION_P) {
 			val lp = window.attributes
-			lp.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+//			lp.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+
+			val clz = WindowManager.LayoutParams::class.java
+			var field: Field? = null
+			try {
+				field = clz.getField("layoutInDisplayCutoutMode")
+				field!!.set(lp, LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES)
+			} catch (e: NoSuchFieldException) {
+				e.printStackTrace()
+			} catch (e: IllegalAccessException) {
+				e.printStackTrace()
+			}
+
 		}
 
 		getWindow().getDecorView().systemUiVisibility = (
@@ -41,25 +54,25 @@ class NotchInfoActivity : AppCompatActivity() {
 
 	fun info() {
 
-		val decorView = window.decorView
-
-		val windowInset = decorView.rootWindowInsets
-		val displayCutout: DisplayCutout? = windowInset.displayCutout
-		println("szw 01 : ${decorView.rootWindowInsets}")
-		println("szw 02 : ${decorView.rootWindowInsets.displayCutout}")
-		Log.e("szw", "SafeArea SafeInsetLeft:" + displayCutout?.safeInsetLeft)
-		Log.e("szw", "SafeArea SafeInsetRight:" + displayCutout?.safeInsetRight)
-		Log.e("szw", "SafeArea SafeInsetTop:" + displayCutout?.safeInsetTop)
-		Log.e("szw", "SafeArea SafeInsetBottom:" + displayCutout?.safeInsetBottom)
-
-		val rects = displayCutout?.boundingRects
-		if (rects == null || rects.size === 0) {
-			Log.e("szw", "Have No Notch")
-		} else {
-			Log.e("szw", "Nonch Count:" + rects.size)
-			for (rect in rects) {
-				Log.e("szw", "Nonch Area：$rect")
-			}
-		}
+		//		val decorView = window.decorView
+		//
+		//		val windowInset = decorView.rootWindowInsets
+		//		val displayCutout: DisplayCutout? = windowInset.displayCutout
+		//		println("szw 01 : ${decorView.rootWindowInsets}")
+		//		println("szw 02 : ${decorView.rootWindowInsets.displayCutout}")
+		//		Log.e("szw", "SafeArea SafeInsetLeft:" + displayCutout?.safeInsetLeft)
+		//		Log.e("szw", "SafeArea SafeInsetRight:" + displayCutout?.safeInsetRight)
+		//		Log.e("szw", "SafeArea SafeInsetTop:" + displayCutout?.safeInsetTop)
+		//		Log.e("szw", "SafeArea SafeInsetBottom:" + displayCutout?.safeInsetBottom)
+		//
+		//		val rects = displayCutout?.boundingRects
+		//		if (rects == null || rects.size === 0) {
+		//			Log.e("szw", "Have No Notch")
+		//		} else {
+		//			Log.e("szw", "Nonch Count:" + rects.size)
+		//			for (rect in rects) {
+		//				Log.e("szw", "Nonch Area：$rect")
+		//			}
+		//		}
 	}
 }
