f5490c5ad9827d3e4ac53157a5018b08f4299cb7
songzhw <songzhw2012@gmail.com>
2019-10-13 10:02:38 -0400

    add androix.security

---
+++ Jetpack+/app/src/main/java/ca/six/jet/security/SecurityDemo.kt
@@ -0,0 +1,11 @@
+package ca.six.jet.security
+
+import android.app.Activity
+import android.os.Bundle
+
+class SecurityDemo : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
