cbb695837f6f7ba17849793162c34eadda7d7525
songzhw <songzhw2012@gmail.com>
2019-10-13 11:30:52 -0400

    save privated text (key,value encrypted)

--- Jetpack+/app/src/main/java/ca/six/jet/MainActivity.kt
+++ Jetpack+/app/src/main/java/ca/six/jet/MainActivity.kt
@@ -1,12 +1,16 @@
 package ca.six.jet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ca.six.jet.security.SecurityDemo
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnSecurity.setOnClickListener { startActivity(Intent(this, SecurityDemo::class.java)) }
     }
 }
--- Jetpack+/app/src/main/java/ca/six/jet/security/SecurityDemo.kt
+++ Jetpack+/app/src/main/java/ca/six/jet/security/SecurityDemo.kt
@@ -2,11 +2,28 @@ package ca.six.jet.security
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
+import ca.six.jet.R
+import kotlinx.android.synthetic.main.activity_security_sp.*
 
 // ''androidx.security:security-crypto' 至少得是minSdk >= 23, 不然会有Manifest Merge Error
 class SecurityDemo : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_security_sp)
 
+        val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        val sp = EncryptedSharedPreferences.create(
+            "normal1", masterKeyAlias, this,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+        val editor = sp.edit()
+
+        btnSaveSp.setOnClickListener {
+            editor.putString("key200", "value200")
+            editor.apply()
+        }
     }
 }
