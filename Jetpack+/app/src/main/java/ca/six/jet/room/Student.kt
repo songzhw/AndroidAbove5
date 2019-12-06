19019cf98a98eb3d42324fca65ede18ba55f2836
songzhw <songzhw2012@gmail.com>
2019-12-06 11:34:35 -0500

    got a funny error: "> Task :app:kaptDebugKotlin FAILED  w: warning: Current JDK version 1.8.0_191-b12 has a bug (https://bugs.openjdk.java.net/browse/JDK-8007720) that prevents Room from being incremental. Consider using JDK 11+ or the embedded JDK shipped with Android Studio 3.5+."

--- Jetpack+/app/src/main/java/ca/six/jet/room/Student.kt
+++ Jetpack+/app/src/main/java/ca/six/jet/room/Student.kt
@@ -18,16 +18,17 @@ interface StudentDao{
     @Query("select * from Student where id = :id")
     fun getStudentById(id: String): Student?
 
+    @Transaction
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(student: Student)
+    suspend fun insert(student: Student)
 
     @Update
-    fun update(student: Student)
+    suspend fun update(student: Student)
 
     @Delete
-    fun delete(student: Student)
+    suspend fun delete(student: Student)
 
     // 其实也可以用@Delete.  @Query其实就是executeSQL()的方式, 可以包罗万象.
     @Query("delete from Student")
-    fun deleteAll()
+    suspend fun deleteAll()
 }
