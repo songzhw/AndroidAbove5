9b1ff96dda36aaf9b6e95d15d7555d7eb5053248
songzhw <songzhw2012@gmail.com>
2019-12-10 08:46:11 -0500

    go back to the pre-packaged db approach

--- Jetpack+/app/src/main/java/ca/six/jet/room/relation/RecipeDatabase.kt
+++ Jetpack+/app/src/main/java/ca/six/jet/room/relation/RecipeDatabase.kt
@@ -84,6 +84,7 @@ object RecipeDatabaseProvider {
     private var db: RecipeDatabase? = null
 
     fun db(context: Context): RecipeDatabase = db ?: synchronized(this) {
+        println("szw db: ${db == null}")
         db ?: build(context).also { db = it }
     }
 
@@ -93,33 +94,7 @@ object RecipeDatabaseProvider {
             RecipeDatabase::class.java,
             "recipes.db"
         )
-            .addCallback(object : RoomDatabase.Callback() {
-                override fun onCreate(db: SupportSQLiteDatabase) {
-                    GlobalScope.launch {
-                        println("szw RecipeDatabase create db")
-                        val dao = db(context).dao()
-                        dao.insertCuisine(Cuisine(null, "西红柿炒蛋"))
-                        dao.insertCuisine(Cuisine(null, "牛肉滑蛋"))
-                        dao.insertCuisine(Cuisine(null, "青椒小炒牛肉"))
-                        dao.insertCuisine(Cuisine(null, "西红柿炖牛腩"))
-
-                        dao.insertIngredient(Ingredient(null, "鸡蛋"))
-                        dao.insertIngredient(Ingredient(null, "西红柿"))
-                        dao.insertIngredient(Ingredient(null, "牛肉"))
-                        dao.insertIngredient(Ingredient(null, "辣椒"))
-
-                        dao.insertJoint(IngredientCuisine(null, 1, 1))
-                        dao.insertJoint(IngredientCuisine(null, 2, 1))
-                        dao.insertJoint(IngredientCuisine(null, 1, 2))
-                        dao.insertJoint(IngredientCuisine(null, 3, 2))
-                        dao.insertJoint(IngredientCuisine(null, 4, 3))
-                        dao.insertJoint(IngredientCuisine(null, 3, 3))
-                        dao.insertJoint(IngredientCuisine(null, 2, 4))
-                        dao.insertJoint(IngredientCuisine(null, 3, 4))
-                        dao.insertJoint(IngredientCuisine(null, 4, 4))
-                    }
-                }
-            })
+            .createFromAsset("databases/recipes.db")
             .build()
 
 }
--- Jetpack+/app/src/main/java/ca/six/jet/room/relation/RecipeDbDemo.kt
+++ Jetpack+/app/src/main/java/ca/six/jet/room/relation/RecipeDbDemo.kt
@@ -16,6 +16,7 @@ class RecipeDbDemo : AppCompatActivity() {
         setContentView(R.layout.activity_room)
 
         val db = RecipeDatabaseProvider.db(this)
+        println("szw db2 : ${db}")
         val dao = db.dao()
 
         val vm = ViewModelProviders.of(this).get(RecipeRoomViewModel::class.java)
@@ -31,27 +32,27 @@ class RecipeDbDemo : AppCompatActivity() {
 class RecipeRoomViewModel : ViewModel() {
 
     fun insert(dao: RecipeDao){
-        viewModelScope.launch {
-            dao.insertCuisine(Cuisine(null, "西红柿炒蛋"))
-            dao.insertCuisine(Cuisine(null, "牛肉滑蛋"))
-            dao.insertCuisine(Cuisine(null, "青椒小炒牛肉"))
-            dao.insertCuisine(Cuisine(null, "西红柿炖牛腩"))
-
-            dao.insertIngredient(Ingredient(null, "鸡蛋"))
-            dao.insertIngredient(Ingredient(null, "西红柿"))
-            dao.insertIngredient(Ingredient(null, "牛肉"))
-            dao.insertIngredient(Ingredient(null, "辣椒"))
-
-            dao.insertJoint(IngredientCuisine(null, 1, 1))
-            dao.insertJoint(IngredientCuisine(null, 2, 1))
-            dao.insertJoint(IngredientCuisine(null, 1, 2))
-            dao.insertJoint(IngredientCuisine(null, 3, 2))
-            dao.insertJoint(IngredientCuisine(null, 4, 3))
-            dao.insertJoint(IngredientCuisine(null, 3, 3))
-            dao.insertJoint(IngredientCuisine(null, 2, 4))
-            dao.insertJoint(IngredientCuisine(null, 3, 4))
-            dao.insertJoint(IngredientCuisine(null, 4, 4))
-        }
+//        viewModelScope.launch {
+//            dao.insertCuisine(Cuisine(null, "西红柿炒蛋"))
+//            dao.insertCuisine(Cuisine(null, "牛肉滑蛋"))
+//            dao.insertCuisine(Cuisine(null, "青椒小炒牛肉"))
+//            dao.insertCuisine(Cuisine(null, "西红柿炖牛腩"))
+//
+//            dao.insertIngredient(Ingredient(null, "鸡蛋"))
+//            dao.insertIngredient(Ingredient(null, "西红柿"))
+//            dao.insertIngredient(Ingredient(null, "牛肉"))
+//            dao.insertIngredient(Ingredient(null, "辣椒"))
+//
+//            dao.insertJoint(IngredientCuisine(null, 1, 1))
+//            dao.insertJoint(IngredientCuisine(null, 2, 1))
+//            dao.insertJoint(IngredientCuisine(null, 1, 2))
+//            dao.insertJoint(IngredientCuisine(null, 3, 2))
+//            dao.insertJoint(IngredientCuisine(null, 4, 3))
+//            dao.insertJoint(IngredientCuisine(null, 3, 3))
+//            dao.insertJoint(IngredientCuisine(null, 2, 4))
+//            dao.insertJoint(IngredientCuisine(null, 3, 4))
+//            dao.insertJoint(IngredientCuisine(null, 4, 4))
+//        }
     }
 
     fun query(dao: RecipeDao) {
