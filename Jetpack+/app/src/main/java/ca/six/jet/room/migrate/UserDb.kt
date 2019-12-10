75a1d37eac31777fc69ff1d00667b82f16daeb0b
songzhw <songzhw2012@gmail.com>
2019-12-10 13:44:59 -0500

    version 2

--- Jetpack+/app/src/main/java/ca/six/jet/room/migrate/UserDb.kt
+++ Jetpack+/app/src/main/java/ca/six/jet/room/migrate/UserDb.kt
@@ -2,13 +2,17 @@ package ca.six.jet.room.migrate
 
 import android.content.Context
 import androidx.room.*
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 
 @Entity
 class User(
     @PrimaryKey(autoGenerate = true) val id: Int,
     val name: String,
-    val timestamp: Long
+    val timestamp: Long,
+    val weight: Int,
+    val sex: String
 )
 
 @Dao
@@ -21,7 +25,7 @@ interface UserDao {
 }
 
 
-@Database(entities = [User::class], version = 1)
+@Database(entities = [User::class], version = 2)
 abstract class UserDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
 }
@@ -40,5 +44,15 @@ object UserDatabaseProvider {
             UserDatabase::class.java,
             "migrates.db"
         )
+            .addMigrations(MIGRATE_1_2)
             .build()
 }
+
+// v2: 新加了int weight,  String sex成员
+val MIGRATE_1_2 = object : Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("ALTER TABLE user ADD COLUMN weight INTEGER NOT NULL DEFAULT 0");
+        database.execSQL("ALTER TABLE user ADD COLUMN sex TEXT");
+    }
+}
+
