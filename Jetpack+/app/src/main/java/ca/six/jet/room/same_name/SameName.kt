5f2be1a1bffe5cca1ea59adf29a0065412b0f158
songzhw <songzhw2012@gmail.com>
2019-12-21 16:23:44 -0500

    room

--- Jetpack+/app/src/main/java/ca/six/jet/room/same_name/SameName.kt
+++ Jetpack+/app/src/main/java/ca/six/jet/room/same_name/SameName.kt
@@ -1,15 +1,57 @@
 package ca.six.jet.room.same_name
 
-import androidx.room.Dao
-import androidx.room.Entity
-import androidx.room.Query
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.viewModelScope
+import androidx.room.*
+import kotlinx.coroutines.launch
 
 @Entity
 data class Sofa(val id: Int)
 
 @Entity
-data class pillow(val id: Int, val sofaId: Int)
+data class Pillow(val id: Int, val sofaId: Int)
+
+data class SofaPillow(@Embedded val sofa: Sofa, @Embedded val pillow: Pillow)
 
 @Dao
 interface SofaDao {
+    @Query("select Sofa.id, Pillow.id from Sofa inner join Pillow on Pillow.sofaId = Sofa.id")
+    suspend fun getAll(): LiveData<List<SofaPillow>>
+
+    @Insert
+    suspend fun insertSofa(sofa: Sofa)
+
+    @Insert
+    suspend fun insertPillow(pillow: Pillow)
+}
+
+@Database(
+    entities = [Sofa::class, Pillow::class],
+    version = 1, exportSchema = false
+)
+abstract class SofaDatabase : RoomDatabase() {
+    abstract fun dao(): SofaDao
+}
+
+
+object SofaDatabaseProvider {
+    @Volatile
+    private var db: SofaDatabase? = null
+
+    fun db(context: Context): SofaDatabase = db ?: synchronized(this) {
+        db ?: build(context).also { db = it }
+    }
+
+    private fun build(context: Context) =
+        Room.databaseBuilder(
+            context.applicationContext,
+            SofaDatabase::class.java,
+            "Sofas.db"
+        )
+            .build()
 }
