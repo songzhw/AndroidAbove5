ce256ff6e85359b3c7793303bd56a21337baa428
songzhw <songzhw2012@gmail.com>
2020-06-29 11:57:28 -0400

    error: "window.decorView.rootWindowInsets must not be null" as it is null in onCreate()

---
+++ Android9x/app/src/main/java/ca/six/ax9/screen/cutout/corner/CornerCutoutSolution2.kt
@@ -0,0 +1,39 @@
+package ca.six.ax9.screen.cutout.corner
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.WindowManager
+import ca.six.ax9.R
+import kotlinx.android.synthetic.main.activity_cutout_corner.*
+
+class CornerCutoutSolution2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cutout_corner)
+
+        val params = window.attributes
+        params.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+
+        window.decorView.systemUiVisibility =
+                // app的绘制区域延伸至StatusBar区域，StatusBar上浮于app之上
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                        // 隐藏StatusBar. 效果如同是加了WindowManager.LayoutParams.FLAG_FULLSCREEN
+                        View.SYSTEM_UI_FLAG_FULLSCREEN
+
+//        window.decorView.setOnApplyWindowInsetsListener { view, insets ->
+//            println("szw insets = $insets")
+//            insets
+//        } //=> 只能看到cutout高132 (因为systemWindoInsets是[0, top=132, 0, 0]的Rect)
+
+            val cutout = window.decorView.rootWindowInsets.displayCutout
+            println("szw cutout = $cutout")
+
+        btnClose.setOnClickListener {
+            println("szw close")
+        }
+    }
+
+}
