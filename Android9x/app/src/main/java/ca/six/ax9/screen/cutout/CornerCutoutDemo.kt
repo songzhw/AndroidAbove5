9d7b2af6e03074564a3ba246dd1504a4a36d775b
songzhw <songzhw2012@gmail.com>
2020-06-29 11:21:25 -0400

    现在水平时的"x"按钮就真的给遮挡了

--- Android9x/app/src/main/java/ca/six/ax9/screen/cutout/CornerCutoutDemo.kt
+++ Android9x/app/src/main/java/ca/six/ax9/screen/cutout/CornerCutoutDemo.kt
@@ -2,6 +2,7 @@ package ca.six.ax9.screen.cutout
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.view.WindowManager
 import ca.six.ax9.R
 import kotlinx.android.synthetic.main.activity_cutout_corner.*
@@ -15,6 +16,12 @@ class CornerCutoutDemo : AppCompatActivity() {
         val params = window.attributes
         params.layoutInDisplayCutoutMode =
                 WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+
+        window.decorView.systemUiVisibility =
+                // app的绘制区域延伸至StatusBar区域，StatusBar上浮于app之上
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                        // 隐藏StatusBar. 效果如同是加了WindowManager.LayoutParams.FLAG_FULLSCREEN
+                        View.SYSTEM_UI_FLAG_FULLSCREEN
 
         btnClose.setOnClickListener {
             println("szw close")
