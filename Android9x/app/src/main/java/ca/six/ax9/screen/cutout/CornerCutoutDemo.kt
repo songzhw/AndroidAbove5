a866cd6ee77efbf2caa43cd8a0f1d8b38824bc2e
songzhw <songzhw2012@gmail.com>
2020-06-29 11:14:54 -0400

    这时cutout区域就是一条白条(原来是黑条), app的内容仍是没有到达cutout区域

--- Android9x/app/src/main/java/ca/six/ax9/screen/cutout/CornerCutoutDemo.kt
+++ Android9x/app/src/main/java/ca/six/ax9/screen/cutout/CornerCutoutDemo.kt
@@ -2,6 +2,7 @@ package ca.six.ax9.screen.cutout
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 import ca.six.ax9.R
 import kotlinx.android.synthetic.main.activity_cutout_corner.*
 
@@ -10,6 +11,10 @@ class CornerCutoutDemo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cutout_corner)
+
+        val params = window.attributes
+        params.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
 
         btnClose.setOnClickListener {
             println("szw close")
