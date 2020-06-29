c4eb451a304dec79767dec303b0b4911bebb48f5
songzhw <songzhw2012@gmail.com>
2020-06-29 11:37:04 -0400

    solution 1

--- Android9x/app/src/main/java/ca/six/ax9/screen/cutout/corner/CornerCutoutSolution1.kt
+++ Android9x/app/src/main/java/ca/six/ax9/screen/cutout/corner/CornerCutoutSolution1.kt
@@ -2,7 +2,7 @@ package ca.six.ax9.screen.cutout.corner
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.WindowManager
+import android.view.View
 import ca.six.ax9.R
 import kotlinx.android.synthetic.main.activity_cutout_corner.*
 
@@ -11,6 +11,7 @@ class CornerCutoutSolution1  : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cutout_corner)
+
 
         btnClose.setOnClickListener {
             println("szw close")
