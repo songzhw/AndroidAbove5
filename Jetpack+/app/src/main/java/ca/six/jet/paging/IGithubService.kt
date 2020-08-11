2c1e9ba76236426d8f7e96537f99e43ce1b127a8
songzhw <songzhw2012@gmail.com>
2020-08-11 16:41:48 -0400

    step 1

---
+++ Jetpack+/app/src/main/java/ca/six/jet/paging/IGithubService.kt
@@ -0,0 +1,32 @@
+package ca.six.jet.paging
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface IGithubService {
+    // page是从1开始的. (若page=0, 效果与page=1一样)
+    @GET("search/repositories?sort=stars")
+    suspend fun searchRepo(
+        @Query("q") query: String,
+        @Query("page") page: Int,
+        @Query("per_page") pageSize: Int
+    )
+}
+
+object GithubService {
+    private val BASE_URL = "https://api.github.com/"
+    val api : IGithubService
+
+    init {
+        val httpClient = OkHttpClient.Builder().build() //这可加interceptor
+        api = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(IGithubService::class.java)
+    }
+}
